fb724d32c96790c408162135d6fa3bc95c99c5d4
Alexey Androsov <aandrosov@yandex-team.ru>
2013-08-14 21:39:19 +0400

    #35 disallowSpaceAfterKeywords: [function] throws error for function decl

--- test/test.disallow-space-after-keywords.js
+++ test/test.disallow-space-after-keywords.js
@@ -15,4 +15,16 @@ describe('rules/disallow-space-after-keywords', function() {
         checker.configure({ disallowSpaceAfterKeywords: ['if'] });
         assert(checker.checkString('if(x) { x++; }').isEmpty());
     });
+    it('should not report space after keyword "function" for named function', function() {
+        checker.configure({ disallowSpaceAfterKeywords: ['function'] });
+        assert(checker.checkString('function a(){}').isEmpty());
+    });
+    it('should report illegal after keyword "function" for anonymous function', function() {
+        checker.configure({ disallowSpaceAfterKeywords: ['function'] });
+        assert(checker.checkString('a.map(function (){})').getErrorCount() === 1);
+    });
+    it('should not report space after keyword "function" for anonymous function', function() {
+        checker.configure({ disallowSpaceAfterKeywords: ['function'] });
+        assert(checker.checkString('a.map(function(){})').isEmpty());
+    });
 });
