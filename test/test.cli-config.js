b4bbb204229920302a64cc16866ddf0f68bbdb2a
Pat Cavit <pcavit@gmail.com>
2014-01-22 13:13:45 -0800

    cli-config module now returns config objects
    - Broke cli-config into multiple methods
    - Tests for all cli-config methods
    - Updates to CLI module to use new functionality
    - Update CLI tests to exercise changes to module
    - Added mocha BDD "afterEach" method to JSHint predefs

--- test/test.cli-config.js
+++ test/test.cli-config.js
@@ -1,28 +1,67 @@
+var path = require('path');
 var assert = require('assert');
-var config = require('../lib/cli-config');
+var configFile = require('../lib/cli-config');
 
 describe('cli-config', function() {
-    it('should find a .jscs.json config file on disk', function() {
-        var file = config(null, './test/data/configs/json');
+    describe('resolve fn', function() {
+        it('should call path.resolve(cwd, file)', function() {
+            assert.equal(
+                configFile.resolve('fooga.js', '/wooga/booga'),
+                path.resolve('/wooga/booga', 'fooga.js')
+            );
+        });
 
-        assert(file.indexOf('.jscs.json') > -1);
+        it('should use process.cwd() if no cwd value was passed', function() {
+            assert.equal(
+                configFile.resolve('fooga.js'),
+                path.resolve(process.cwd(), 'fooga.js')
+            );
+        });
     });
 
-    it('should find a .jscsrc config file on disk', function() {
-        var file = config(null, './test/data/configs/jscsrc');
+    describe('find fn', function() {
+        it('should find a .jscs.json config file on disk', function() {
+            var file = configFile.find(null, './test/data/configs/json');
 
-        assert(file.indexOf('.jscsrc') > -1);
+            assert(file.indexOf('.jscs.json') > -1);
+        });
+
+        it('should find a .jscsrc config file on disk', function() {
+            var file = configFile.find(null, './test/data/configs/jscsrc');
+
+            assert(file.indexOf('.jscsrc') > -1);
+        });
+
+        it('should find a custom config file on disk', function() {
+            var file = configFile.find('config.js', './test/data/configs/custom');
+
+            assert(file.indexOf('config.js') > -1);
+        });
+
+        it('should prefer .jscsrc over .jscs.json', function() {
+            var file = configFile.find(null, './test/data/configs/mixed');
+
+            assert(file.indexOf('.jscsrc') > -1);
+        });
+
+        it('should not fallback to defaults if custom config is missing', function() {
+            var file = configFile.find('config.js', './test/data/configs/mixed');
+
+            assert.equal(file, undefined);
+        });
     });
 
-    it('should find a custom config file on disk', function() {
-        var file = config('config.js', './test/data/configs/custom');
+    describe('load fn', function() {
+        it('should require() a file off disk', function() {
+            var config = configFile.load('config.js', './test/data/configs/custom');
 
-        assert(file.indexOf('config.js') > -1);
-    });
+            assert(typeof config === 'object');
+        });
 
-    it('should prefer .jscsrc over .jscs.json', function() {
-        var file = config(null, './test/data/configs/mixed');
+        it('should return false if the config can\'t be found', function() {
+            var config = configFile.load(null, './test/data/configs/custom');
 
-        assert(file.indexOf('.jscsrc') > -1);
+            assert.equal(config, false);
+        });
     });
 });
