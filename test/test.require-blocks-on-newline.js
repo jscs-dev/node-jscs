7ee39396d9b053e19a02eb538c757fede5a20ee2
Famlam <fam.lam@live.nl>
2014-02-02 01:32:49 +0100

    Move checker.configure(...) line to beforeeach

--- test/test.require-blocks-on-newline.js
+++ test/test.require-blocks-on-newline.js
@@ -6,25 +6,21 @@ describe('rules/require-blocks-on-newline', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
+        checker.configure({ requireBlocksOnNewline: true });
     });
     it('should report missing newline after opening brace', function() {
-        checker.configure({ requireBlocksOnNewline: true });
         assert(checker.checkString('if (true) {abc();\n}').getErrorCount() === 1);
     });
     it('should report missing newline before closing brace', function() {
-        checker.configure({ requireBlocksOnNewline: true });
         assert(checker.checkString('if (true) {\nabc();}').getErrorCount() === 1);
     });
     it('should report missing newlines in both cases', function() {
-        checker.configure({ requireBlocksOnNewline: true });
         assert(checker.checkString('if (true) {abc();}').getErrorCount() === 2);
     });
     it('should not report with no spaces', function() {
-        checker.configure({ requireBlocksOnNewline: true });
         assert(checker.checkString('if (true) {\nabc();\n}').isEmpty());
     });
     it('should not report empty function definitions', function() {
-        checker.configure({ requireBlocksOnNewline: true });
         assert(checker.checkString('var a = function() {};').isEmpty());
     });
 });
