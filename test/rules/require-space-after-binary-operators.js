86e14ae7c265674787464b67d9f564921bb317b9
Oleg Gaidarenko <markelog@gmail.com>
2014-05-15 01:00:15 +0400

    Improve requireSpaceAfterBinaryOperators rule
    Add check for assignment to requireSpaceAfterBinaryOperators rule

--- test/rules/require-space-after-binary-operators.js
+++ test/rules/require-space-after-binary-operators.js
@@ -45,5 +45,13 @@ describe('rules/require-space-after-binary-operators', function() {
         checker.configure({ requireSpaceAfterBinaryOperators: [','] });
         assert(checker.checkString('({a: 1, b: 2})').isEmpty());
     });
+    it('should report assignment operator for "a = b"', function() {
+        checker.configure({ requireSpaceAfterBinaryOperators: ['='] });
+        assert(checker.checkString('a = b').isEmpty());
+    });
+    it('should not report assignment operator for "a=b"', function() {
+        checker.configure({ requireSpaceAfterBinaryOperators: ['='] });
+        assert(checker.checkString('a=b').getErrorCount() === 1);
+    });
 
 });
