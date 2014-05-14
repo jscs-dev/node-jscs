cc027af84c4f5fb1ef7bddc1760ffbab1a832aab
Oleg Gaidarenko <markelog@gmail.com>
2014-05-15 00:54:50 +0400

    Improve disallowSpaceAfterBinaryOperators rule
    Add check for assignment to disallowSpaceAfterBinaryOperators rule

--- test/rules/disallow-space-after-binary-operators.js
+++ test/rules/disallow-space-after-binary-operators.js
@@ -41,5 +41,13 @@ describe('rules/disallow-space-after-binary-operators', function() {
         checker.configure({ disallowSpaceAfterBinaryOperators: [','] });
         assert(checker.checkString('({a: 1, b: 2})').getErrorCount() === 1);
     });
+    it('should report assignment operator for "a = b"', function() {
+        checker.configure({ disallowSpaceAfterBinaryOperators: ['='] });
+        assert(checker.checkString('a = b').getErrorCount() === 1);
+    });
+    it('should not report assignment operator for "a=b"', function() {
+        checker.configure({ disallowSpaceAfterBinaryOperators: ['='] });
+        assert(checker.checkString('a=b').isEmpty());
+    });
 
 });
