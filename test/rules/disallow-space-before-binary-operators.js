b3107d772ee09ebf6bfa05f605c5d96de4deadfc
Oleg Gaidarenko <markelog@gmail.com>
2014-05-16 17:35:03 +0400

    Improve disallowSpaceBeforeBinaryOperators rule
    Add support and tests for all binary operators

--- test/rules/disallow-space-before-binary-operators.js
+++ test/rules/disallow-space-before-binary-operators.js
@@ -2,52 +2,65 @@ var Checker = require('../../lib/checker');
 var assert = require('assert');
 
 describe('rules/disallow-space-before-binary-operators', function() {
-
     var checker;
+    var operators = [
+
+        // assignment operators
+        '=', '+=', '-=', '*=', '/=', '%=', '<<=', '>>=', '>>>=',
+        '&=', '|=', '^=', '+=',
+
+        '+', '-', '*', '/', '%', '<<', '>>', '>>>', '&',
+        '|', '^', '&&', '||', '===', '==', '>=',
+        '<=', '<', '>', '!=', '!==',
+
+        ','
+    ];
+
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
     });
 
-    it('should not report sticky operator for "2+2"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['+'] });
-        assert(checker.checkString('var x = 2+2;').isEmpty());
+    operators.forEach(function( operator ) {
+        var sticked = 'var test; test' + operator + '2';
+        var stickedWithParenthesis = 'var test; (test)' + operator + '(2)';
+
+        var notSticked = 'var test; test ' + operator + ' 2';
+        var notStickedWithParenthesis = 'var test; (test) ' + operator + ' (2)';
+
+        it('should not report sticky operator for ' + sticked, function() {
+            checker.configure({ disallowSpaceBeforeBinaryOperators: [operator] });
+            assert(checker.checkString(sticked).isEmpty());
+        });
+        it('should report sticky operator for ' + notSticked, function() {
+            checker.configure({ disallowSpaceBeforeBinaryOperators: [operator] });
+            assert(checker.checkString(notSticked).getErrorCount() === 1);
+        });
+        it('should not report sticky operator for ' + stickedWithParenthesis, function() {
+            checker.configure({ disallowSpaceBeforeBinaryOperators: [operator] });
+            assert(checker.checkString(stickedWithParenthesis).isEmpty());
+        });
+        it('should report sticky operator for ' + notStickedWithParenthesis, function() {
+            checker.configure({ disallowSpaceBeforeBinaryOperators: [operator] });
+            assert(checker.checkString(notStickedWithParenthesis).getErrorCount() === 1);
+        });
     });
-    it('should report separated operator for "2 +2"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['+'] });
-        assert(checker.checkString('var x = 2 +2;').getErrorCount() === 1);
+
+    it('should not report sticky operator for ({ test:2 })', function() {
+        checker.configure({ disallowSpaceBeforeBinaryOperators: [':'] });
+        assert(checker.checkString('({ test:2 })').isEmpty());
     });
-    it('should not report sticky operator for "a = (a)+ b"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['+'] });
-        assert(checker.checkString('a = (a)+ b').isEmpty());
+    it('should report sticky operator for ({ test :2 })', function() {
+        checker.configure({ disallowSpaceBeforeBinaryOperators: [':'] });
+        assert(checker.checkString('({ test : 2 })').getErrorCount() === 1);
     });
-    it('should report separated operator for "a = (a) + (b)"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['+'] });
-        assert(checker.checkString('a = (a) + (b)').getErrorCount() === 1);
+    it('should not report sticky operator ":" in ternary', function() {
+        checker.configure({ disallowSpaceBeforeBinaryOperators: [':'] });
+        assert(checker.checkString('test ? 1 : 2').isEmpty());
     });
-    it('should report separated operator for "2, 2"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: [','] });
-        assert(checker.checkString('2, 2').isEmpty());
-    });
-    it('should report separated operator for "2,2"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: [','] });
-        assert(checker.checkString('2 , 2').getErrorCount() === 1);
-    });
-    it('should report separated operator for "({a: 1 , b: 2})"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: [','] });
-        assert(checker.checkString('({a: 1 , b: 2})').getErrorCount() === 1);
-    });
-    it('should not report separated operator for "({a: 1, b: 2})"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: [','] });
-        assert(checker.checkString('({a: 1, b: 2})').isEmpty());
-    });
-    it('should report assignment operator for "a = b"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['='] });
-        assert(checker.checkString('a = b').getErrorCount() === 1);
-    });
-    it('should not report assignment operator for "a=b"', function() {
-        checker.configure({ disallowSpaceBeforeBinaryOperators: ['='] });
-        assert(checker.checkString('a=b').isEmpty());
+    it('should not report sticky operator "?" in ternary', function() {
+        checker.configure({ disallowSpaceBeforeBinaryOperators: ['?'] });
+        assert(checker.checkString('test ? 1 : 2').isEmpty());
     });
     it('should not report assignment operator for "a = b" without option', function() {
         checker.configure({ disallowSpaceBeforeBinaryOperators: [','] });
