da8842ddb4eac70f6df77c50aa0885fa4c18a05b
Oleg Gaidarenko <markelog@gmail.com>
2014-05-22 20:45:40 +0400

    Improve requireOperatorBeforeLineBreak rule
    Fixes #393: should not report for unary operators

--- test/rules/require-operator-before-line-break.js
+++ test/rules/require-operator-before-line-break.js
@@ -38,6 +38,18 @@ describe('rules/require-operator-before-line-break', function() {
         checker.configure({ requireOperatorBeforeLineBreak: true });
         assert(checker.checkString('var x = y \n? a : b').getErrorCount() === 1);
     });
+    it('should report newline before colon with true value', function() {
+        checker.configure({ requireOperatorBeforeLineBreak: true });
+        assert(checker.checkString('({ test \n : 1 })').getErrorCount() === 1);
+    });
+    it('should not report newline before colon without option', function() {
+        checker.configure({ requireOperatorBeforeLineBreak: ['?'] });
+        assert(checker.checkString('({ test \n : 1 })').isEmpty());
+    });
+    it('should not report newline before ternary without option', function() {
+        checker.configure({ requireOperatorBeforeLineBreak: [':'] });
+        assert(checker.checkString('var x = y \n? a : b').isEmpty());
+    });
     it('should report newline before ternary', function() {
         checker.configure({ requireOperatorBeforeLineBreak: ['?'] });
         assert(checker.checkString('var x = y \n? a : b').getErrorCount() === 1);
@@ -45,5 +57,10 @@ describe('rules/require-operator-before-line-break', function() {
     it('should not report newline after ternary', function() {
         checker.configure({ requireOperatorBeforeLineBreak: ['?'] });
         assert(checker.checkString('var x = y ?\n a : b').isEmpty());
+    });
+
+    it('should not report newline for the unary operator', function() {
+        checker.configure({ requireOperatorBeforeLineBreak: ['-'] });
+        assert(checker.checkString('[\n-1, \n2]').isEmpty());
     });
 });
