71d482ac368db5d7be28b92290e0ae17a89cce95
Mike Sherov <mike.sherov@gmail.com>
2014-06-10 08:51:53 -0400

    Sticked Operators Rules: Deprecate in favor of more specific rules
    * output deprecation notice with list of rules to use instead
    * update documentation

--- test/rules/disallow-right-sticked-operators.js
+++ test/rules/disallow-right-sticked-operators.js
@@ -6,13 +6,19 @@ describe('rules/disallow-right-sticked-operators', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
+        checker.configure({ disallowRightStickedOperators: ['+'] });
     });
-    it('should report sticky operator', function() {
-        checker.configure({ disallowRightStickedOperators: ['!'] });
-        assert(checker.checkString('var x = !y;').getErrorCount() === 1);
-    });
-    it('should not report separated operator', function() {
-        checker.configure({ disallowRightStickedOperators: ['!'] });
-        assert(checker.checkString('var x = ! y;').isEmpty());
+    it('should output correct deprecation notice', function() {
+        var errors = checker.checkString('var a = b +c; var a = b +c;').getErrorList();
+        assert(errors.length === 1);
+
+        var error = errors[0];
+        assert(error.line === 1 && error.column === 0);
+        assert(error.message === 'The disallowRightStickedOperators rule is no longer supported.' +
+            '\nPlease use the following rules instead:' +
+            '\n' +
+            '\nrequireSpaceAfterBinaryOperators' +
+            '\nrequireSpaceAfterPrefixUnaryOperators' +
+            '\nrequireSpacesInConditionalExpression');
     });
 });
