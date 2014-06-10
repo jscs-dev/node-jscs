53c4fcaea8a11b2553cdfe5faec8651e9e440775
Oleg Gaidarenko <markelog@gmail.com>
2014-06-10 13:40:55 +0400

    Additions for disallowSpacesInsideObjectBrackets
    * Make code more clear
    
    * Add one more test
    
    * Structurize tests

--- test/rules/disallow-spaces-inside-object-brackets.js
+++ test/rules/disallow-spaces-inside-object-brackets.js
@@ -8,65 +8,71 @@ describe('rules/disallow-spaces-inside-object-brackets', function() {
         checker.registerDefaultRules();
     });
 
-    describe('true value or "all"', function() {
-        it('should report illegal space after opening brace, with true value', function() {
+    describe('true value', function() {
+        beforeEach(function() {
             checker.configure({ disallowSpacesInsideObjectBrackets: true });
+        });
+
+        it('should report illegal space after opening brace, with true value', function() {
             assert(checker.checkString('var x = { a: 1};').getErrorCount() === 1);
         });
         it('should report illegal space before closing brace, with true value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: true });
             assert(checker.checkString('var x = {a: 1 };').getErrorCount() === 1);
         });
         it('should report illegal space in both cases, with true value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: true });
             assert(checker.checkString('var x = { a: 1 };').getErrorCount() === 2);
         });
         it('should report illegal space for nested braces too, with true value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: true });
             assert(checker.checkString('var x = { test: { a: 1 } };').getErrorCount() === 4);
         });
         it('should not report with no spaces, with true value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: true });
             assert(checker.checkString('var x = {a: 1};').isEmpty());
+        });
+    });
+
+    describe('"all"', function() {
+        beforeEach(function() {
+            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
         });
 
         it('should report illegal space after opening brace, with "all" value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
             assert(checker.checkString('var x = { a: 1};').getErrorCount() === 1);
         });
         it('should report illegal space before closing brace, with "all" value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
             assert(checker.checkString('var x = {a: 1 };').getErrorCount() === 1);
         });
         it('should report illegal space in both cases, with "all" value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
             assert(checker.checkString('var x = { a: 1 };').getErrorCount() === 2);
         });
         it('should report illegal space for nested braces too, with "all" value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
             assert(checker.checkString('var x = { test: { a: 1 } };').getErrorCount() === 4);
         });
         it('should not report with no spaces, with "all" value', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'all' });
             assert(checker.checkString('var x = {a: 1};').isEmpty());
         });
     });
 
     describe('"nested"', function() {
-        it('should report illegal space after opening brace for nested object', function() {
+        beforeEach(function() {
             checker.configure({ disallowSpacesInsideObjectBrackets: 'nested' });
+        });
+
+        it('should not report illegal space for not nested object', function() {
+            assert(checker.checkString('var x = { 1 : 2 };').isEmpty());
+        });
+        it('should report illegal space after opening brace for nested object', function() {
             assert(checker.checkString('var x = {1: { 1 : 2}};').getErrorCount() === 1);
         });
         it('should report illegal space before closing brace for nested object', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'nested' });
             assert(checker.checkString('var x = {1: {1 : 2 }};').getErrorCount() === 1);
         });
         it('should report illegal space in both cases for nested object', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'nested' });
             assert(checker.checkString('var x = {1: { 1 : 2 }};').getErrorCount() === 2);
         });
+        it('should report illegal space in both cases for multiple nested object', function() {
+            assert(checker.checkString('var x = {1: { 1 : 2 }, 2: { 3 : 4 }};').getErrorCount() === 4);
+        });
         it('should not report illegal space in both cases for nested object', function() {
-            checker.configure({ disallowSpacesInsideObjectBrackets: 'nested' });
             assert(checker.checkString('var x = { 1: {1 : 2} };').isEmpty());
         });
     });
