2050a276bedf111103e5c38d7f23ddb342910141
Oleg Gaidarenko <markelog@gmail.com>
2014-06-17 15:16:37 +0400

    Improve Checker#checkDirectory method
    Make it aware of "fileExtensions" option
    
    Ref #445

--- test/options/file-extensions.js
+++ test/options/file-extensions.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 var Checker = require('../../lib/checker');
 var assert = require('assert');
 
@@ -10,7 +12,6 @@ describe('options/file-extensions', function() {
     });
 
     describe('default config', function() {
-
         beforeEach(function() {
             checker.configure({
                 disallowKeywords: ['with']
@@ -20,21 +21,21 @@ describe('options/file-extensions', function() {
         it('should not report any errors for non-matching extensions with default config', function() {
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.jsx') === null);
         });
-
         it('should report errors for matching extensions with default config', function() {
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.js') !== null);
         });
-
         it('should report errors for matching extensions (case insensitive) with default config', function() {
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions-2.jS') !== null);
         });
-
+        it('should report errors for matching extensions (case insensitive) in directory with default config', function(done) {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 2);
+                done();
+            });
+        });
     });
 
     describe('custom config', function() {
-
         it('should not report any errors for non-matching extensions with custom config', function() {
             checker.configure({
                 fileExtensions: ['.jsx'],
@@ -43,69 +44,86 @@ describe('options/file-extensions', function() {
 
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.js') === null);
         });
-
-        it('should report errors for matching extensions with custom config', function() {
+        it('should report errors for matching extensions with custom config', function(done) {
             checker.configure({
                 fileExtensions: ['.jsx'],
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.jsx') !== null);
-        });
 
-        it('should report errors for matching extensions (case insensitive) with custom config', function() {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 1);
+                done();
+            });
+        });
+        it('should report errors for matching extensions (case insensitive) with custom config', function(done) {
             checker.configure({
                 fileExtensions: ['.JS'],
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions-2.jS') !== null);
-        });
 
-        it('should report errors for matching extensions (case insensitive) with string value', function() {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 2);
+                done();
+            });
+        });
+        it('should report errors for matching extensions (case insensitive) with string value', function(done) {
             checker.configure({
                 fileExtensions: '.JS',
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions-2.jS') !== null);
-        });
 
-        it('should report errors for matching extensions with custom config with multiple extensions', function() {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 2);
+                done();
+            });
+        });
+        it('should report errors for matching extensions with custom config with multiple extensions', function(done) {
             checker.configure({
                 fileExtensions: ['.js', '.jsx'],
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.js') !== null);
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.jsx') !== null);
-        });
 
-        it('should report errors for matching extensions with Array *', function() {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 3);
+                done();
+            });
+        });
+        it('should report errors for matching extensions with Array *', function(done) {
             checker.configure({
                 fileExtensions: ['*'],
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.js') !== null);
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.jsx') !== null);
-        });
 
-        it('should report errors for matching extensions with string *', function() {
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 3);
+                done();
+            });
+        });
+        it('should report errors for matching extensions with string *', function(done) {
             checker.configure({
                 fileExtensions: '*',
                 disallowKeywords: ['with']
             });
 
-            // errors
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.js') !== null);
             assert(checker.checkFile('./test/data/configs/fileExtensions/file-extensions.jsx') !== null);
+
+            checker.checkDirectory('./test/data/configs/fileExtensions').then(function(errors) {
+                assert(errors.length === 3);
+                done();
+            });
         });
     });
-
 });
