8069c3a338374a545e8897940757c346358bb106
Oleg Gaidarenko <markelog@gmail.com>
2014-06-28 22:11:13 +0400

    Correct deal with exclusion and extensions
    Fixes #438

--- test/options/exclude-files.js
+++ test/options/exclude-files.js
@@ -1,6 +1,7 @@
 var Checker = require('../../lib/checker');
 var configFile = require('../../lib/cli-config');
 var assert = require('assert');
+var Vow = require('vow');
 
 describe('options/exclude-files', function() {
     var checker;
@@ -11,90 +12,166 @@ describe('options/exclude-files', function() {
     });
 
     describe('use config in script', function() {
-        it('should not report any errors', function() {
+        it('should not report any errors', function(done) {
             checker.configure({
                 excludeFiles: ['test/data/configs/excludeFiles/exclude-files.js'],
                 disallowKeywords: ['with']
             });
 
-            assert(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js') === null);
+            checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js').then(function(errors) {
+                assert(errors.length === 0);
+                done();
+            });
         });
 
-        it('should allow patterns to match filenames starting with a period', function() {
+        it('should allow patterns to match filenames starting with a period', function(done) {
             checker.configure({
                 excludeFiles: ['test/data/configs/excludeFiles/**'],
                 disallowKeywords: ['with']
             });
 
-            assert(checker.checkFile('./test/data/configs/excludeFiles/.withdot/error.js') === null);
+            checker.checkFile('./test/data/configs/excludeFiles/.withdot/error.js').then(function(errors) {
+                assert(errors.length === 0);
+                done();
+            });
         });
 
-        it('should resolve pattern to process.cwd', function() {
+        it('should resolve pattern to process.cwd', function(done) {
+            var results = [];
             checker.configure({
                 excludeFiles: ['test/data/exclude-files.js'],
                 disallowKeywords: ['with']
             });
 
             // errors
-            assert(checker.checkFile('./test/data/configs/excludeFiles/script.js') !== null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/script.js') !== null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/script.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/script.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
 
-        it('should resolve pattern to process.cwd', function() {
+        it('should resolve pattern to process.cwd', function(done) {
+            var results = [];
             checker.configure({
                 excludeFiles: ['test/data/exclude-files.js'],
                 disallowKeywords: ['with']
             });
 
             // errors
-            assert(checker.checkFile('./test/data/configs/excludeFiles/script.js') !== null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/script.js') !== null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/script.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/script.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
     });
 
-    describe('should resolve pattern relative to config file', function() {
-        it('(pattern: *.js)', function() {
+    describe('should resolve pattern relative to config file', function(done) {
+        it('(pattern: *.js)', function(done) {
+            var results = [];
             checker.configure(configFile.load('./test/data/configs/excludeFiles/test1.jscs.json'));
 
             // ok
-            assert(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js') === null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js').then(function(errors) {
+                assert(errors.length === 0);
+            }));
 
             // errors
-            assert(checker.checkFile('./test/data/exclude-files.js') !== null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js') !== null);
+            results.push(checker.checkFile('./test/data/exclude-files.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js')
+                .then(function(errors) {
+                    assert(errors.length === 0);
+                })
+            );
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
 
-        it('(pattern: exclude-files.js)', function() {
+        it('(pattern: exclude-files.js)', function(done) {
+            var results = [];
             checker.configure(configFile.load('./test/data/configs/excludeFiles/test2.jscs.json'));
 
             // ok
-            assert(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js') === null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js').then(function(errors) {
+                assert(errors.length === 0);
+            }));
 
             // errors
-            assert(checker.checkFile('./test/data/exclude-files.js') !== null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js') !== null);
+            results.push(checker.checkFile('./test/data/exclude-files.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js')
+                .then(function(errors) {
+                    assert(errors.length === 0);
+                })
+            );
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
 
-        it('(pattern: */exclude-files.js)', function() {
+        it('(pattern: */exclude-files.js)', function(done) {
+            var results = [];
             checker.configure(configFile.load('./test/data/configs/excludeFiles/test3.jscs.json'));
 
             // ok
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js') === null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js')
+                .then(function(errors) {
+                    assert(errors.length === 0);
+                })
+            );
 
             // errors
-            assert(checker.checkFile('./test/data/exclude-files.js') !== null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js') !== null);
+            results.push(checker.checkFile('./test/data/exclude-files.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
 
-        it('(pattern: ../**/exclude-files.js)', function() {
+        it('(pattern: ../**/exclude-files.js)', function(done) {
+            var results = [];
             checker.configure(configFile.load('./test/data/configs/excludeFiles/test4.jscs.json'));
 
             // ok
-            assert(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js') === null);
-            assert(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js') === null);
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/nested/exclude-files.js')
+                .then(function(errors) {
+                    assert(errors.length === 0);
+                })
+            );
+            results.push(checker.checkFile('./test/data/configs/excludeFiles/exclude-files.js').then(function(errors) {
+                assert(errors.length === 0);
+            }));
 
             // errors
-            assert(checker.checkFile('./test/data/exclude-files.js') !== null);
+            results.push(checker.checkFile('./test/data/exclude-files.js').then(function(errors) {
+                assert(errors.length > 0);
+            }));
+
+            Vow.allResolved(results).then(function() {
+                done();
+            });
         });
     });
 
