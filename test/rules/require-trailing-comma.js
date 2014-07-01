56f61b3b3650bb0d50e7b09adeb04b2dcc387930
Joel Kemp <mrjoelkemp@gmail.com>
2014-07-01 14:25:39 -0400

    requireTrailingComma: Avoids false positives from non object/array literal definitions. Fixes #368.

--- test/rules/require-trailing-comma.js
+++ test/rules/require-trailing-comma.js
@@ -22,6 +22,7 @@ describe('rules/require-trailing-comma', function() {
         assert(checker.checkString('var x = {a: "a", b: "b",\n}').getErrorCount() === 0);
         assert(checker.checkString('var x = {a: "a", b: "b"}').getErrorCount() === 1);
         assert(checker.checkString('var x = {a: "a", b: "b"\n}').getErrorCount() === 1);
+        assert(checker.checkString('function foo() {\nreturn {a: "a"};\n}').getErrorCount() === 1);
     });
 
     it('should report trailing comma required in array', function() {
@@ -29,6 +30,15 @@ describe('rules/require-trailing-comma', function() {
         assert(checker.checkString('var x = [1, 2,\n]').getErrorCount() === 0);
         assert(checker.checkString('var x = [1, 2]').getErrorCount() === 1);
         assert(checker.checkString('var x = [1, 2\n]').getErrorCount() === 1);
+        assert(checker.checkString('function foo() {\nreturn [1, 2];\n}').getErrorCount() === 1);
     });
 
+    it('should not report block scopes (#368)', function() {
+        assert(checker.checkString('if(true) {\nconsole.log(\'Hello World\');\n}').getErrorCount() === 0);
+        assert(checker.checkString('function add(a, b) {\nreturn a + b;\n}').getErrorCount() === 0);
+    });
+
+    it('should not report array access (#368)', function () {
+        assert(checker.checkString('var foo = [\'Hello World\',\n];\nvar bar = foo[0];').getErrorCount() === 0);
+    });
 });
