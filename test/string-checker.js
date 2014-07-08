927507ecc49b9e7eada3b48c0c44e510858334c9
OpenShift guest <jeroen-876@hotmail.com>
2014-07-08 04:26:18 -0400

    first version of autoformat. This is still WIP

--- test/string-checker.js
+++ test/string-checker.js
@@ -100,4 +100,175 @@ describe('modules/string-checker', function() {
             assert(error === undefined);
         });
     });
+
+    describe('formatString', function() {
+        it('requireCurlyBraces', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireCurlyBraces: ['while', 'do']
+            });
+            assert(checker.checkString(
+                checker.formatString(   'while(i<l) \ni++;\n' +
+                                        'do\ni++;\nwhile (i<l);' +
+                                        'try {\n i++;}\ncatch(e)\n{ e=e;}')
+            ).isEmpty());
+        });
+        it('requireSpaceAfterKeywords', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceAfterKeywords: ['if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch']
+            });
+            assert(checker.checkString(
+                checker.formatString(   'if(i<l){\n i++;\n}else{\n i--;\n}')
+            ).isEmpty());
+        });
+        it('requireSpacesInFunctionExpression', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpacesInFunctionExpression: {beforeOpeningCurlyBrace: true}
+            });
+            assert(checker.checkString(
+                checker.formatString('function test(){}')
+            ).isEmpty());
+        });
+        it('requirePaddingNewlinesInBlocks', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requirePaddingNewlinesInBlocks: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){i++;}')
+            ).isEmpty());
+        });
+        it('requireSpacesInsideObjectBrackets', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpacesInsideObjectBrackets: 'all'
+            });
+            assert(checker.checkString(
+                checker.formatString('var a={a:1,b:2};')
+            ).isEmpty());
+        });
+        it('requireSpacesInsideArrayBrackets', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpacesInsideArrayBrackets: 'allButNested'
+            });
+            assert(checker.checkString(
+                checker.formatString('var a=[1,2,3];')
+            ).isEmpty());
+        });
+        it('requireSpaceBeforeBlockStatements', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceBeforeBlockStatements: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){i++;}')
+            ).isEmpty());
+        });
+        it('requireLineFeedAtFileEnd', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireLineFeedAtFileEnd: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){i++;}')
+            ).isEmpty());
+        });
+        it('validateLineBreaks', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                validateLineBreaks: 'LF'
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){\ri++;\r}\r')
+            ).isEmpty());
+        });
+        it('validateIndentation', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                validateIndentation: '\t'
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){\ni++;\nif(i<l){\ni++;\n}\n }\n')
+            ).isEmpty());
+        });
+        it('requireSpaceAfterPrefixUnaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceAfterPrefixUnaryOperators: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){++i;if(i<l){+i;}}')
+            ).isEmpty());
+        });
+
+        it('requireSpaceBeforePostfixUnaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceBeforePostfixUnaryOperators: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){i++;if(i<l){i--;}}')
+            ).isEmpty());
+        });
+
+        it('requireSpaceBeforeBinaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceBeforeBinaryOperators: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){l=l+i;if(i<l){l+=i;}}')
+            ).isEmpty());
+        });
+
+        it('requireSpaceAfterBinaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpaceAfterBinaryOperators: true
+            });
+            assert(checker.checkString(
+                checker.formatString('if(i<l){l=l+i;if(i<l){l+=i;}}')
+            ).isEmpty());
+        });
+
+        it('disallowSpaceBeforeBinaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                disallowSpaceBeforeBinaryOperators: [',']
+            });
+            assert(checker.checkString(
+                checker.formatString('var a=[1 ,2  ,3]; ')
+            ).isEmpty());
+        });
+
+        it('disallowSpaceBeforeBinaryOperators', function() {
+            var checker = new Checker();
+            checker.registerDefaultRules();
+            checker.configure({
+                requireSpacesInConditionalExpression: {afterTest: true, beforeConsequent: true, afterConsequent: true, beforeAlternate: true}
+            });
+            assert(checker.checkString(
+                checker.formatString('var a=a?b:c; ')
+            ).isEmpty());
+        });
+
+    });
 });
