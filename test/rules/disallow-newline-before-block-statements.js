43d50013347c558babc03cc409a479e38ad15a31
cipiripper <cipiripper@gmail.com>
2014-07-24 16:39:24 +0200

    Removed unnecessary return check

--- test/rules/disallow-newline-before-block-statements.js
+++ test/rules/disallow-newline-before-block-statements.js
@@ -31,13 +31,13 @@ describe('rules/disallow-newline-before-block-statements', function() {
         });
 
         it('should not report disallowed newline for object definitions', function() {
-            assert(checker.checkString('function test(){var obj = \n{a:1,\nb:2,\nc:3\n};\n\n return \n{\nval:1\n};\n}')
+            assert(checker.checkString('function test(){var obj = \n{a:1,\nb:2,\nc:3\n};\n\n return {\nval:1\n};\n}')
                 .isEmpty());
         });
 
         it('should report disallowed newline only for function definition block statement', function() {
             assert(
-            checker.checkString('function test()\n{var obj = \n{a:1,\nb:2,\nc:3\n};\n\n return \n{\nval:1\n};\n}')
+            checker.checkString('function test()\n{var obj = \n{a:1,\nb:2,\nc:3\n};\n\n return {\nval:1\n};\n}')
                 .getErrorCount() === 1);
         });
 
@@ -48,7 +48,7 @@ describe('rules/disallow-newline-before-block-statements', function() {
 
         it('should not report disallowed newline', function() {
             assert(checker.checkString(
-                'function test(){\nif(true){\nreturn \n{\nval:1\n}\n}\nvar obj = \n{a:1,\nb:2,\nc:3\n};\n}')
+                'function test(){\nif(true){\nreturn {\nval:1\n}\n}\nvar obj = \n{a:1,\nb:2,\nc:3\n};\n}')
                 .isEmpty());
         });
     });
