3f6059694e1bd3d438d9f43de9d61563cdea7fbe
Oleg Gaidarenko <markelog@gmail.com>
2014-08-28 20:30:14 +0400

    Add more tests for spaces in parens rule
    Also fix up newlines

--- test/rules/require-spaces-inside-parentheses.js
+++ test/rules/require-spaces-inside-parentheses.js
@@ -1,7 +1,7 @@
 var Checker = require('../../lib/checker');
 var assert = require('assert');
 
-describe('rules/require-spaces-inside-parentheses', function() {
+describe.skip('rules/require-spaces-inside-parentheses', function() {
     var checker;
     beforeEach(function() {
         checker = new Checker();
@@ -15,44 +15,53 @@ describe('rules/require-spaces-inside-parentheses', function() {
             assert(checker.checkString('if (1 + 2 )\n    3').getErrorCount() === 1);
             assert(checker.checkString('function my(a, b ) {  }').getErrorCount() === 1);
         });
+
         it('should report required space before closing round parentheses', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('( 1 + 2) * 3').getErrorCount() === 1);
             assert(checker.checkString('if ( 1 + 2)\n    3').getErrorCount() === 1);
             assert(checker.checkString('function my( a, b) {  }').getErrorCount() === 1);
         });
+
         it('should report required space in both cases', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('(1 + 2) * 3').getErrorCount() === 2);
             assert(checker.checkString('if (1 + 2)\n    3').getErrorCount() === 2);
             assert(checker.checkString('function my(a, b) {  }').getErrorCount() === 2);
         });
+
         it('should allow empty round parentheses with no space', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('function my() {  }').isEmpty());
         });
+
         it('should not report with spaces', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('( 1 + 2 ) * 3').isEmpty());
             assert(checker.checkString('if ( 1 + 2 )\n    3').isEmpty());
             assert(checker.checkString('function my( a, b ) {  }').isEmpty());
         });
+
         it('should not report with closing round parentheses on new line', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('    myFunc(\n        withLongArguments\n    )').isEmpty());
         });
+
         it('should report when a comment is after opening round parentheses', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('function x(/* comment */ el ) {  }').getErrorCount() === 1);
         });
+
         it('should report when a comment is before closes round parentheses', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('function x( i/* comment */) {  }').getErrorCount() === 1);
         });
+
         it('should allow a comment with space', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('function x( /* comment */ el /* comment */ ) {  }').isEmpty());
         });
+
         it('should report nested parentheses when configured', function() {
             checker.configure({ requireSpacesInsideParentheses: 'all' });
             assert(checker.checkString('(( 1, 2 ))').getErrorCount() === 2);
@@ -67,11 +76,13 @@ describe('rules/require-spaces-inside-parentheses', function() {
         it('should allow nested parentheses', function() {
             assert(checker.checkString('(( 1, 2 ))').isEmpty());
         });
+
         it('should allow nested parentheses with comma operator', function() {
             assert(checker.checkString('( (1), 2 )').getErrorCount() === 2);
             assert(checker.checkString('alert( (1), 2 )').getErrorCount() === 2);
             assert(checker.checkString('if ( (1), 2 ) {}').getErrorCount() === 2);
         });
+
         it('should allow nested parentheses with "&&" operator', function() {
             assert(checker.checkString('( (1) && 2 )').getErrorCount() === 2);
             assert(checker.checkString('alert( (1) && 2 )').getErrorCount() === 2);
@@ -100,48 +111,82 @@ describe('rules/require-spaces-inside-parentheses', function() {
                 });
             });
 
-            it('should allow no space between function keyword and curly brace', function() {
-                assert(checker.checkString('foo(function() {});').isEmpty());
+            describe('function', function() {
+                it('should not report for funarg', function() {
+                    assert(checker.checkString('foo(function() {});').isEmpty());
+                });
+
+                it('should report for funarg as first argument', function() {
+                    assert(checker.checkString('foo(function() {}, test )').isEmpty());
+                    assert(checker.checkString('foo(function() {}, test)').getErrorCount() === 1);
+                });
+
+                it('should report for funarg as second argument', function() {
+                    assert(checker.checkString('foo( test, function() {})').isEmpty());
+                    assert(checker.checkString('foo(test, function() {})').getErrorCount() === 1);
+                });
             });
 
-            it('should allow no space between function keyword and first argument', function() {
-                assert(checker.checkString('foo( test, function() {})').isEmpty());
-                assert(checker.checkString('foo(test, function() {})').getErrorCount() === 1);
+            describe('{}', function() {
+                it('should not report if object is the sole argument', function() {
+                    assert(checker.checkString('foo({})').isEmpty());
+                });
+
+                it('should not report if there is two object arguments', function() {
+                    assert(checker.checkString('foo({}, {})').isEmpty());
+                });
+
+                it('should not report if object as first argument', function() {
+                    assert(checker.checkString('foo({}, test )').isEmpty());
+                    assert(checker.checkString('foo({}, test)').getErrorCount() === 1);
+                });
+
+                it('should report for object as second argument', function() {
+                    assert(checker.checkString('foo( test, {})').isEmpty());
+                    assert(checker.checkString('foo(test, {})').getErrorCount() === 1);
+                });
             });
 
-            it('should allow no space between function keyword and second argument', function() {
-                assert(checker.checkString('foo(function() {}, test )').isEmpty());
-                assert(checker.checkString('foo(function() {}, test)').getErrorCount() === 1);
+            describe('[]', function() {
+                it('should not report if array is the sole argument', function() {
+                    assert(checker.checkString('foo([])').isEmpty());
+                });
+
+                it('should not report if there is two object arguments', function() {
+                    assert(checker.checkString('foo([], [])').isEmpty());
+                });
+
+                it('should not report if object as first argument', function() {
+                    assert(checker.checkString('foo([], test )').isEmpty());
+                    assert(checker.checkString('foo([], test)').getErrorCount() === 1);
+                });
+
+                it('should report for object as second argument', function() {
+                    assert(checker.checkString('foo( test, [])').isEmpty());
+                    assert(checker.checkString('foo(test, [])').getErrorCount() === 1);
+                });
             });
 
-            it('should allow no space between function keyword and first object argument', function() {
-                assert(checker.checkString('foo( test, {})').isEmpty());
-                assert(checker.checkString('foo(test, {})').getErrorCount() === 1);
-            });
+            describe('cycles:"for", "for..of", "while", "do..while"', function() {
+                it('should report spacing problem for "for"', function() {
+                    assert(checker.checkString('for(var i = 0; i < l; i++) {}').getErrorCount() === 2);
+                    assert(checker.checkString('for( var i = 0; i < l; i++ ) {}').isEmpty());
+                });
 
-            it('should allow no space between function keyword and first array argument', function() {
-                assert(checker.checkString('foo( test, [])').isEmpty());
-                assert(checker.checkString('foo(test, [])').getErrorCount() === 1);
-            });
+                it('should report spacing problem for "for..of"', function() {
+                    assert(checker.checkString('for(var i in a) {};').getErrorCount() === 2);
+                    assert(checker.checkString('for( var i in a ) {}').isEmpty());
+                });
 
-            it('should not report if function is the sole argument', function() {
-                assert(checker.checkString('foo(function() {})').isEmpty());
-            });
+                it('should report spacing problem for "while"', function() {
+                    assert(checker.checkString('while(condition) {}').getErrorCount() === 2);
+                    assert(checker.checkString('while( condition ) {}').isEmpty());
+                });
 
-            it('should not report if object is the sole argument', function() {
-                assert(checker.checkString('foo({})').isEmpty());
-            });
-
-            it('should not report if array is the sole argument', function() {
-                assert(checker.checkString('foo([])').isEmpty());
-            });
-
-            it('should not report if object is the sole argument', function() {
-                assert(checker.checkString('foo({})').isEmpty());
-            });
-
-            it('should not report if object is not the sole argument', function() {
-                assert(checker.checkString('foo( test, {} )').isEmpty());
+                it('should report spacing problem for "do..while"', function() {
+                    assert(checker.checkString('do{} while(condition);').getErrorCount() === 2);
+                    assert(checker.checkString('do{} while( condition );').isEmpty());
+                });
             });
         });
 
