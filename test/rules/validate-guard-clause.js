f166b2ae16c2be7657578fe6b1b14fdf003e6995
Benjy Cui <benjytrys@gmail.com>
2014-08-28 08:57:11 +0800

    Add rule validateGuardClause and its test case.

---
+++ test/rules/validate-guard-clause.js
@@ -0,0 +1,93 @@
+var Checker = require('../../lib/checker');
+var assert = require('assert');
+
+describe('/rules/validate-guard-clause', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+    });
+
+    it('should report missing guard clause braces', function() {
+        checker.configure({ validateGuardClause: { requireCurlyBraces: true } });
+        assert(
+            checker.checkString(
+                'function test() { if (x) return; }'
+            ).getErrorCount() === 1
+        );
+    });
+
+    it('should not report guard clause with braces', function() {
+        checker.configure({ validateGuardClause: { requireCurlyBraces: true } });
+        assert(
+            checker.checkString(
+                'function test() { if (x) { return; } }'
+            ).isEmpty()
+        );
+    });
+
+    it('should report guard clause with braces', function() {
+        checker.configure({ validateGuardClause: { disallowCurlyBraces: true } });
+        assert(
+            checker.checkString(
+                'function test() { if (x) { return; } }'
+            ).getErrorCount() === 1
+        );
+    });
+
+    it('should not report guard clause without braces', function() {
+        checker.configure({ validateGuardClause: { disallowCurlyBraces: true } });
+        assert(
+            checker.checkString(
+                'function test() { if (x) return; }'
+            ).isEmpty()
+        );
+    });
+
+    it('should report guard clause which is not written in one line', function() {
+        checker.configure({ validateGuardClause: { requireInOneLine: true } });
+        assert(
+            checker.checkString(
+                'function test() {\n' +
+                    'if (x)\n' +
+                        'return;' +
+                '}'
+            ).getErrorCount() === 1
+        );
+    });
+
+    it('should not report guard clause which is written in one line', function() {
+        checker.configure({ validateGuardClause: { requireInOneLine: true } });
+        assert(
+            checker.checkString(
+                'function test() {\n' +
+                    'if (x) return;' +
+                '}'
+            ).isEmpty()
+        );
+    });
+
+    it('should report guard clause which is in one line', function() {
+        checker.configure({ validateGuardClause: { disallowInOneLine: true } });
+        assert(
+            checker.checkString(
+                'function test() {\n' +
+                    'if (x) return;' +
+                '}'
+            ).getErrorCount() === 1
+        );
+    });
+
+    it('should not report guard clause which is not in one line', function() {
+        checker.configure({ validateGuardClause: { disallowInOneLine: true } });
+        assert(
+            checker.checkString(
+                'function test() {\n' +
+                    'if (x)\n' +
+                        'return;' +
+                '}'
+            ).isEmpty()
+        );
+    });
+});
