87fb915c1e6964c72848b4f68e007f07f1860c08
Mike Sherov <mike.sherov@gmail.com>
2014-09-02 08:36:08 -0400

    Add direct unit tests for options/preset

--- test/options/preset.js
+++ test/options/preset.js
@@ -1,4 +1,5 @@
 var Checker = require('../../lib/checker');
+var preset = require('../../lib/options/preset');
 var assert = require('assert');
 
 describe('options/preset', function() {
@@ -20,9 +21,7 @@ describe('options/preset', function() {
      * @param  {String} presetName
      */
     function testPreset(presetName) {
-        var desc = describe;
-
-        desc(presetName + ' preset', function() {
+        describe(presetName + ' preset', function() {
             var checker = new Checker();
             var preset = require('../../presets/' + presetName);
 
@@ -45,4 +44,43 @@ describe('options/preset', function() {
             });
         });
     }
+
+    describe('getDoesNotExistError', function() {
+        it('returns the correct error message', function() {
+            assert(preset.getDoesNotExistError('foo') === 'Preset "foo" does not exist');
+        });
+    });
+
+    describe('exists', function() {
+        it('returns true for existing presets', function() {
+            assert(preset.exists('jquery'));
+        });
+
+        it('returns false for non-existant presets', function() {
+            assert(!preset.exists('aPresetThatWillNeverExist'));
+        });
+    });
+
+    describe('extend', function() {
+        it('returns true if preset not present in config', function() {
+            assert(preset.extend({ not: 'real' }));
+        });
+
+        it('returns false if provided preset is not a real preset', function() {
+            assert(!preset.extend({ preset: 'aPresetThatWillNeverExist' }));
+        });
+
+        it('removes the preset key from the config, and add its rules to the config', function() {
+            var config = {
+                preset: 'jquery',
+                fakeRule: true
+            };
+
+            preset.extend(config);
+
+            assert(!config.preset);
+            assert(config.requireOperatorBeforeLineBreak);
+            assert(config.fakeRule);
+        });
+    });
 });
