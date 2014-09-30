235b29b7d7a95b973336e2246a5d07258ec7b960
Jindrich Besta <besta.jindrich@ifortuna.cz>
2014-09-30 23:34:11 +0200

    disallowSpaceBeforeObjectKeys: fixed rule for parenthesised property value
    Fixed rule so that it detects (and reports) if there is a space before
    property value and property value is parenthesised expression.
    
    Fixes: #652

--- test/rules/disallow-space-before-object-values.js
+++ test/rules/disallow-space-before-object-values.js
@@ -13,9 +13,25 @@ describe('rules/disallow-space-before-object-values', function() {
     it('should report with space after keys colons', function() {
         assert.equal(checker.checkString('var x = { a:1, b: 2 };').getErrorCount(), 1, 'one error is found');
         assert.equal(checker.checkString('var x = { abc : 1, b: 2 };').getErrorCount(), 2, 'two errors are found');
+        assert.equal(
+            checker.checkString('var x = { abc:(true), z: (function() { return _z > 0; }) };').getErrorCount(),
+            1,
+            'one error is found'
+        );
+        assert.equal(
+            checker.checkString('var x = { abc : (true), b: ("1")};').getErrorCount(),
+            2,
+            'two errors are found'
+        );
+    });
+
+    it('should not report with no space after keys colons and parenthesised expression in property value', function() {
+        assert(checker.checkString('var x = { a:(1 > 2)};').isEmpty());
+        assert(checker.checkString('var x = { 0x7f   :(y?(z ? 1: 2):(3)) };').isEmpty());
     });
 
     it('should not report with no space after keys colons', function() {
         assert(checker.checkString('var x = { a:1, bcd :2 };').isEmpty());
     });
+
 });
