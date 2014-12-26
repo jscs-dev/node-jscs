257224c0dc833ffe6d7c39a8fcf919d8a89d8a7a
Alexej Yaroshevich <alex@qfox.ru>
2014-12-26 19:58:03 +0400

    disallowSpacesInConditionalExpression: notice parentheses
    * Use newly introduced methods of jsfile module
    
    * Add tests for parentheses
    
    * Add tests for recursive ternary
    
    Ref #762
    Ref gh-859

--- test/rules/disallow-spaces-in-conditional-expression.js
+++ test/rules/disallow-spaces-in-conditional-expression.js
@@ -32,6 +32,14 @@ describe('rules/disallow-spaces-in-conditional-expression', function() {
         it('should report space after test in Conditional Expression', function() {
             assert(checker.checkString('var x = a ?b : c;').getErrorCount() === 1);
         });
+
+        it('should not report no space after test in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = (a)? b : c;').isEmpty());
+        });
+
+        it('should report space after test in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = (a) ? b : c;').getErrorCount() === 1);
+        });
     });
 
     describe('beforeConsequent', function() {
@@ -57,6 +65,14 @@ describe('rules/disallow-spaces-in-conditional-expression', function() {
 
         it('should report space after test in Conditional Expression', function() {
             assert(checker.checkString('var x = a? b : c;').getErrorCount() === 1);
+        });
+
+        it('should not report no space before consequent in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a ?(b) : c;').isEmpty());
+        });
+
+        it('should report space after test for consequent in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a? (b) : c;').getErrorCount() === 1);
         });
     });
 
@@ -84,6 +100,14 @@ describe('rules/disallow-spaces-in-conditional-expression', function() {
         it('should report space after consequent in Conditional Expression', function() {
             assert(checker.checkString('var x = a ? b :c;').getErrorCount() === 1);
         });
+
+        it('should not report no space after consequent in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a ? (b): c;').isEmpty());
+        });
+
+        it('should report space after consequent in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a ? (b) : c;').getErrorCount() === 1);
+        });
     });
 
     describe('beforeAlternate', function() {
@@ -110,6 +134,14 @@ describe('rules/disallow-spaces-in-conditional-expression', function() {
         it('should report space before alternate in Conditional Expression', function() {
             assert(checker.checkString('var x = a ? b: c;').getErrorCount() === 1);
         });
+
+        it('should not report no space before alternate in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a ? b :(c);').isEmpty());
+        });
+
+        it('should report space before alternate in parens in Conditional Expression', function() {
+            assert(checker.checkString('var x = a ? b : (c);').getErrorCount() === 1);
+        });
     });
 
     describe('true', function() {
@@ -123,6 +155,14 @@ describe('rules/disallow-spaces-in-conditional-expression', function() {
 
         it('should report all spaces in Conditional Expression', function() {
             assert(checker.checkString('var x = a ? b : c;').getErrorCount() === 4);
+        });
+
+        it('should not report no spaces in recursive Conditional Expression', function() {
+            assert(checker.checkString('var x = t1?c1t2?c2:a2:a1t3?c3:a3;').isEmpty());
+        });
+
+        it('should report all spaces in recursive Conditional Expression', function() {
+            assert(checker.checkString('var x = t1 ? c1t2 ? c2 : a2 : a1t3 ? c3 : a3;').getErrorCount() === 12);
         });
     });
 });
