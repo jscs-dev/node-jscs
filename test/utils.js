d00923040b6e90cbe58f13669b1c9873ba9cfd76
Alexej Yaroshevich <alex@qfox.ru>
2014-12-31 20:18:46 +0400

    utils: added isSnakeCased, trimUnderscores methods and tests
    Ref #866

--- test/utils.js
+++ test/utils.js
@@ -44,4 +44,35 @@ describe('modules/utils', function() {
             assert(!utils.isValidIdentifierName('invalid-name'));
         });
     });
+
+    describe('isSnakeCased', function() {
+        it('should return true for snake cased', function() {
+            assert(utils.isSnakeCased('valid_Name'));
+            assert(utils.isSnakeCased('valid_Name_1'));
+        });
+
+        it('should return false for camel cased and others', function() {
+            assert(!utils.isSnakeCased('invalidName1'));
+            assert(!utils.isSnakeCased('invalidName'));
+            assert(!utils.isSnakeCased('1invalidName'));
+            assert(!utils.isSnakeCased('invalid-name'));
+            assert(!utils.isSnakeCased('$'));
+        });
+    });
+
+    describe('trimUnderscores', function() {
+        it('should trim trailing underscores', function() {
+            assert.equal(utils.trimUnderscores('__snake_cased'), 'snake_cased');
+            assert.equal(utils.trimUnderscores('snake_cased__'), 'snake_cased');
+            assert.equal(utils.trimUnderscores('__snake_cased__'), 'snake_cased');
+            assert.equal(utils.trimUnderscores('__camelCased'), 'camelCased');
+            assert.equal(utils.trimUnderscores('camelCased__'), 'camelCased');
+            assert.equal(utils.trimUnderscores('__camelCased__'), 'camelCased');
+        });
+
+        it('should not trim underscores for underscores only', function() {
+            assert.equal(utils.trimUnderscores('_'), '_');
+            assert.equal(utils.trimUnderscores('__'), '__');
+        });
+    });
 });
