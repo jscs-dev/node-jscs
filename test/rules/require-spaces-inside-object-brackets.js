2a2f39efe1de300bdbbce76b538a0da1da6f11d7
Oleg Gaidarenko <markelog@gmail.com>
2015-01-09 07:57:38 +0300

    requireSpacesInsideObjectBrackets: implement "allExcept" option
    Aside from main improvement:
    
    * Simplify logic check
    
    * Use assertion API
    
    * Add couple more tests for main logic
    
    Ref #721

--- test/rules/require-spaces-inside-object-brackets.js
+++ test/rules/require-spaces-inside-object-brackets.js
@@ -34,6 +34,15 @@ describe('rules/require-spaces-inside-object-brackets', function() {
         it('should report anything for empty object', function() {
             assert(checker.checkString('var x = {};').isEmpty());
         });
+        it('should report for function value', function() {
+            assert(checker.checkString('var x = { a: function() {}};').getErrorCount() === 1);
+        });
+        it('should report for array literal', function() {
+            assert(checker.checkString('var x = { a: []};').getErrorCount() === 1);
+        });
+        it('should report for parentheses', function() {
+            assert(checker.checkString('var x = { a: (1)};').getErrorCount() === 1);
+        });
     });
 
     describe('"allButNested"', function() {
@@ -59,8 +68,36 @@ describe('rules/require-spaces-inside-object-brackets', function() {
         it('should not report illegal space between closing braces for nested object', function() {
             assert(checker.checkString('var x = { a: { b: 1 } };').isEmpty());
         });
-        it('should report anything for empty object', function() {
+        it('should report not anything for empty object', function() {
             assert(checker.checkString('var x = { a: {}};').isEmpty());
+        });
+    });
+
+    describe('exceptions', function() {
+        describe('"}", "]", ")"', function() {
+            beforeEach(function() {
+                checker.configure({
+                    requireSpacesInsideObjectBrackets: {
+                        allExcept: ['}', ']', ')']
+                    }
+                });
+            });
+
+            it('should report missing spaces', function() {
+                assert(checker.checkString('var x = {a: 1};').getErrorCount() === 2);
+            });
+            it('should not report for function', function() {
+                assert(checker.checkString('var x = { a: function() {}};').isEmpty());
+            });
+            it('should not report for array literal', function() {
+                assert(checker.checkString('var x = { a: []};').isEmpty());
+            });
+            it('should not report for parentheses', function() {
+                assert(checker.checkString('var x = { a: (1)};').isEmpty());
+            });
+            it('should not report for object literal', function() {
+                assert(checker.checkString('var x = { a: { b: 1 }};').isEmpty());
+            });
         });
     });
 });
