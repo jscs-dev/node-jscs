954fe96eba0cd20c65642559f3dd5b185a50a2db
Oleg Gaidarenko <markelog@gmail.com>
2015-01-10 18:10:30 +0300

    requireSpacesInsideArrayBrackets: implement "allExcept" option
    Aside from main improvement:
    
    * Simplify logic check
    
    * Use assertion API
    
    * Add couple more tests for main logic
    
    * Fix accessor array case i.e. as expression statement
    
    Fixes #111
    Ref #722
    Ref #429

--- test/rules/require-spaces-inside-array-brackets.js
+++ test/rules/require-spaces-inside-array-brackets.js
@@ -12,7 +12,10 @@ describe('rules/require-spaces-inside-array-brackets', function() {
         beforeEach(function() {
             checker.configure({ requireSpacesInsideArrayBrackets: 'all' });
         });
-        it.skip('should report missing spaces for array and expression statement (#429)', function() {
+        it('should report missing spaces for array accessor', function() {
+            assert(checker.checkString('var x = []; x[0]').getErrorCount() === 2);
+        });
+        it('should report missing spaces for array and expression statement (#429)', function() {
             assert(checker.checkString('[ 1 ][0];').getErrorCount() === 2);
         });
         it('should report missing space after opening brace', function() {
@@ -42,6 +45,12 @@ describe('rules/require-spaces-inside-array-brackets', function() {
         beforeEach(function() {
             checker.configure({ requireSpacesInsideArrayBrackets: 'allButNested' });
         });
+        it('should report missing spaces for array accessor', function() {
+            assert(checker.checkString('var x = []; x[0]').getErrorCount() === 2);
+        });
+        it('should report missing spaces for array and expression statement (#429)', function() {
+            assert(checker.checkString('[ 1 ][0];').getErrorCount() === 2);
+        });
         it('should report missing space after opening brace', function() {
             assert(checker.checkString('var x = [1 ];').getErrorCount() === 1);
         });
@@ -60,8 +69,41 @@ describe('rules/require-spaces-inside-array-brackets', function() {
         it('should report anything for empty array', function() {
             assert(checker.checkString('[[]];').isEmpty());
         });
-        it.skip('should report missing spaces for array and expression statement with nested array (#429)', function() {
+        it('should report missing spaces for array and expression statement with nested array (#429)', function() {
             assert(checker.checkString('[ [ 1 ][0] ];').getErrorCount() === 2);
+        });
+    });
+
+    describe('exceptions', function() {
+        it('should not report missing space for parentheses', function() {
+            checker.configure({
+                requireSpacesInsideArrayBrackets: {
+                    allExcept: ['{', '}']
+                }
+            });
+
+            assert(checker.checkString('var x = [1];').getErrorCount() === 2);
+            assert(checker.checkString('var x = [{ a: 1 }];').isEmpty());
+        });
+        it('should report missing space for the array brackets', function() {
+            checker.configure({
+                requireSpacesInsideArrayBrackets: {
+                    allExcept: ['[', ']']
+                }
+            });
+
+            assert(checker.checkString('var x = [{}];').getErrorCount() === 2);
+            assert(checker.checkString('var x = [[]];').isEmpty());
+        });
+        it('should not report missing space in both cases', function() {
+            checker.configure({
+                requireSpacesInsideArrayBrackets: {
+                    allExcept: ['(', ')']
+                }
+            });
+
+            assert(checker.checkString('var x = [{ a: 1 }];').getErrorCount() === 2);
+            assert(checker.checkString('var x = [(1)];').isEmpty());
         });
     });
 });
