388971f25cf11e0ea9bd5919193f7e29c7576da9
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2015-01-11 11:38:57 -0700

    disableTrailingWhitespace: new rule value - ignoreEmptyLines
    Closes gh-900
    Fixes #719

--- test/rules/disallow-trailing-whitespace.js
+++ test/rules/disallow-trailing-whitespace.js
@@ -7,26 +7,46 @@ describe('rules/disallow-trailing-whitespace', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
-        checker.configure({ disallowTrailingWhitespace: true });
     });
 
-    it('should report trailing spaces', function() {
-        assert(checker.checkString('var x; ').getErrorCount() === 1);
+    describe('option value true', function() {
+        beforeEach(function() {
+            checker.configure({ disallowTrailingWhitespace: true });
+        });
+
+        it('should report trailing spaces', function() {
+            assert(checker.checkString('var x; ').getErrorCount() === 1);
+        });
+
+        it('should report trailing tabs', function() {
+            assert(checker.checkString('var x;\t').getErrorCount() === 1);
+        });
+
+        it('should report trailing whitespace on empty lines', function() {
+            assert(checker.checkString('if(a){\n\tb=c;\n\t\n}').getErrorCount() === 1);
+        });
+
+        it('should report once for each line', function() {
+            assert(checker.checkString('var x;\t\nvar y;\t').getErrorCount() === 2);
+        });
+
+        it('should not report multiline strings with trailing whitespace', function() {
+            assert(checker.checkString('var x = \' \\\n \';').isEmpty());
+        });
+
+        it('should not report when there is no trailing whitespace', function() {
+            assert(checker.checkString('var x;').isEmpty());
+        });
     });
 
-    it('should report trailing tabs', function() {
-        assert(checker.checkString('var x;\t').getErrorCount() === 1);
+    describe('ignoreEmptyLines', function() {
+        beforeEach(function() {
+            checker.configure({ disallowTrailingWhitespace: 'ignoreEmptyLines' });
+        });
+
+        it('should not report trailing whitespace on empty lines', function() {
+            assert(checker.checkString('if(a){\n\tb=c;\n\t\n}').isEmpty());
+        });
     });
 
-    it('should report once for each line', function() {
-        assert(checker.checkString('var x;\t\nvar y;\t').getErrorCount() === 2);
-    });
-
-    it('should not report multiline strings with trailing whitespace', function() {
-        assert(checker.checkString('var x = \' \\\n \';').isEmpty());
-    });
-
-    it('should not report when there is no trailing whitespace', function() {
-        assert(checker.checkString('var x;').isEmpty());
-    });
 });
