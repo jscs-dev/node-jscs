dc8f01cc9822515dd92c50484fd32b6e76c18cf4
Oleg Gaidarenko <markelog@gmail.com>
2015-01-12 22:18:29 +0300

    requireSpaceBeforeBlockStatements: fix for else statement
    Ref b0b756640a7c0088866651173a05a9a94905a5f0

--- test/rules/require-space-before-block-statements.js
+++ test/rules/require-space-before-block-statements.js
@@ -36,4 +36,62 @@ describe('rules/require-space-before-block-statements', function() {
         assert(checker.checkString('var a = {};').getErrorCount() === 0);
         assert(checker.checkString('var a = {id:5};').getErrorCount() === 0);
     });
+
+    it('should missing space for control structures with multiple branches', function() {
+        assert(checker.checkString(
+          'if (true) {\n' +
+          '    var a = false;\n' +
+          '} else {\n' +
+          '    var b = false;\n' +
+          '}').isEmpty(), 'correct if and else');
+
+        assert(checker.checkString(
+          'if (true) {\n' +
+          '    var a = false;\n' +
+          '}\n' +
+          'else{\n' +
+          '    var b = false;\n' +
+          '}').getErrorCount() === 1, 'correct if, incorrect else');
+
+        assert(checker.checkString(
+          'if (true) {\n' +
+          '    var a = false;\n' +
+          '}\n' +
+          'else if (true) {\n' +
+          '    var b = false;\n' +
+          '}\n' +
+          'else{\n' +
+          '    var c = false;\n' +
+          '}').getErrorCount() === 1, 'correct if & else if, incorrect else');
+
+        assert(checker.checkString(
+          'if (true){\n' +
+          '    var a = false;\n' +
+          '}\n' +
+          'else {\n' +
+          '    var b = false;\n' +
+          '}').getErrorCount() === 1, 'incorrect if, correct else');
+
+        assert(checker.checkString(
+          'if (true){\n' +
+          '    var a = false;\n' +
+          '}else{\n' +
+          '    var b = false;\n' +
+          '}').getErrorCount() === 2, 'no spaces: incorrect if and else');
+
+        assert(checker.checkString(
+          'if (true) {\n' +
+          '    var a = false;\n' +
+          '}\n' +
+          'else {\n' +
+          '    var b = false;\n' +
+          '}').getErrorCount() === 0, 'stroustrup: correct if and else');
+
+        assert(checker.checkString(
+          'if (true) {\n' +
+          '    var a = false;\n' +
+          '} else {\n' +
+          '    var b = false;\n' +
+          '}').getErrorCount() === 0, '1tbs: correct if and else');
+    });
 });
