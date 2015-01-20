129b792ae9dd338398b2113c13836b8937da58cd
Sam L'ecuyer <slecuyer@yahoo-inc.com>
2015-01-20 13:11:49 -0800

    requireCapitalizedConstructors: accept list of exempt constructors
    This allows requireCapitalizedConstructors to accept either `true`
    or a list of constructors that are allowed to remain uncapitalized
    By passing a list of identifiers, `true` is assumed.
    
    fixes #390

--- test/rules/require-capitalized-constructors.js
+++ test/rules/require-capitalized-constructors.js
@@ -4,25 +4,50 @@ var assert = require('assert');
 describe('rules/require-capitalized-constructors', function() {
     var checker;
 
+    function baseCases() {
+        it('should report uncapitalized construction', function() {
+            assert(checker.checkString('var x = new y();').getErrorCount() === 1);
+        });
+
+        it('should not report capitalized construction', function() {
+            assert(checker.checkString('var x = new Y();').isEmpty());
+        });
+
+        it('should not report member expression construction', function() {
+            assert(checker.checkString('var x = new ns.y();').isEmpty());
+        });
+
+        it('should not report construction with "this" keyword', function() {
+            assert(checker.checkString('var x = new this();').isEmpty());
+        });
+    }
+
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
-        checker.configure({ requireCapitalizedConstructors: true });
     });
 
-    it('should report uncapitalized construction', function() {
-        assert(checker.checkString('var x = new y();').getErrorCount() === 1);
+    describe('with `true` value', function() {
+        beforeEach(function() {
+            checker.configure({ requireCapitalizedConstructors: true });
+        });
+
+        baseCases();
     });
 
-    it('should not report capitalized construction', function() {
-        assert(checker.checkString('var x = new Y();').isEmpty());
-    });
+    describe('with `allExcept` value', function() {
+        beforeEach(function() {
+            checker.configure({
+                requireCapitalizedConstructors: {
+                    allExcept: ['somethingNative']
+                }
+            });
+        });
 
-    it('should not report member expression construction', function() {
-        assert(checker.checkString('var x = new ns.y();').isEmpty());
-    });
+        baseCases();
 
-    it('should not report construction with "this" keyword', function() {
-        assert(checker.checkString('var x = new this();').isEmpty());
+        it('should not report exempted construction', function() {
+            assert(checker.checkString('var x = new somethingNative();').isEmpty());
+        });
     });
 });
