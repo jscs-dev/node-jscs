059d53a69534a70e0414eb3fe99eb86e1f739952
Dave Hilton <daveh@qualtrics.com>
2015-02-05 11:59:13 -0700

    NewlineBeforeBlockStatement: allow settings per block statement type
    Allows the (require|disallow)NewlineBeforeBlockStatement rules to optionally
    accept an array of block statement "types" to enforce, rather than merely a
    global "true" setting.
    
    This allows for much greater flexibility and fine-grained control of style settings.
    
    Closes gh-1005

--- test/specs/rules/disallow-newline-before-block-statements.js
+++ test/specs/rules/disallow-newline-before-block-statements.js
@@ -10,7 +10,7 @@ describe('rules/disallow-newline-before-block-statements', function() {
         checker.registerDefaultRules();
     });
 
-    describe('option value true', function() {
+    describe('with option value true -', function() {
         beforeEach(function() {
             checker.configure({ disallowNewlineBeforeBlockStatements: true });
         });
@@ -58,6 +58,273 @@ describe('rules/disallow-newline-before-block-statements', function() {
 
         it('should not throw error if opening parentheses is first symbol in the file', function() {
             assert(checker.checkString('{ test: 1 }').isEmpty());
+        });
+    });
+
+    describe('with option value array - ', function() {
+        describe('"if" blocks', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+            });
+
+            it('should report extra newlines when configured with "if"', function() {
+                assert(checker.checkString('if(i == 0)\n{\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "if" and newline not added', function() {
+                assert(checker.checkString('if(i == 0) {\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "if"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['else'] });
+                assert(checker.checkString('if(i == 0)\n{\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"else" and "else if" blocks', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['else'] });
+            });
+
+            it('should report extra newlines when configured with "else"', function() {
+                assert(checker.checkString('if(i == 0) {\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}\nelse\n{\n\tx--;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "else" and newline not added', function() {
+                assert(checker.checkString('if(i == 0) {\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}\nelse {\n\tx--;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "else"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+                assert(checker.checkString('if(i == 0) {\n\tx++;\n\ty = {\n\t\tb: "1"\n\t};\n}\nelse\n{\n\tx--;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"for" loops', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+            });
+
+            it('should report extra newlines when configured with "for"', function() {
+                assert(checker.checkString('for (var i = 0, len = 10; i < 10; ++i)\n{\n\tx++;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "for" and newline not added', function() {
+                assert(checker.checkString('for (var i = 0, len = 10; i < 10; ++i) {\n\tx++;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when note configured with "for"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+                assert(checker.checkString('for (var i = 0, len = 10; i < 10; ++i)\n{\n\tx++;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"for...in" loops', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+            });
+
+            it('should report extra newlines when configured with "for"', function() {
+                assert(checker.checkString('for (var i in x)\n{\n\ty++;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "for" and newline not added', function() {
+                assert(checker.checkString('for (var i in x) {\n\ty++;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "for"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+                assert(checker.checkString('for (var i in x)\n{\n\ty++;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('function declarations', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['function'] });
+            });
+
+            it('should report extra newlines when configured with "function"', function() {
+                assert(checker.checkString('function myFunc(y)\n{\n\ty++;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "function" and newline not added', function() {
+                assert(checker.checkString('function myFunc(y) {\n\ty++;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "function"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+                assert(checker.checkString('function myFunc(y)\n{\n\ty++;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('function statements', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['function'] });
+            });
+
+            it('should report extra newlines when configured with "function"', function() {
+                assert(checker.checkString('var z = function(y)\n{\n\ty++;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "function" and newline not added', function() {
+                assert(checker.checkString('var z = function(y) {\n\ty++;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "function"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['if'] });
+                assert(checker.checkString('var z = function(y)\n{\n\ty++;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"try" blocks', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['try'] });
+            });
+
+            it('should report extra newlines when configured with "try"', function() {
+                assert(checker.checkString('try\n{\n\ty++;\n} catch(e) {\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "try" and newline not added', function() {
+                assert(checker.checkString('try {\n\ty++;\n} catch(e) {\n}\nfinally\n{\n\tq = 5;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "try"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+                assert(checker.checkString('try\n{\n\ty++;\n} catch(e) {\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"catch" blocks', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['catch'] });
+            });
+
+            it('should report extra newlines when configured with "catch"', function() {
+                assert(checker.checkString('try {\n\ty++;\n} catch(e)\n{\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "catch" and newline not added', function() {
+                assert(checker.checkString('try {\n\ty++;\n} catch(e) {\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "catch"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+                assert(checker.checkString('try {\n\ty++;\n} catch(e)\n{\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"finally" blocks', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['finally'] });
+            });
+
+            it('should report extra newlines when configured with "finally"', function() {
+                assert(checker.checkString('try {\n\ty++;\n} catch(e) {\n} finally\n{\n\tq = 5;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "finally" and newline not added', function() {
+                assert(checker.checkString('try {\n\ty++;\n} catch(e) {\n} finally {\n\tq = 5;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "finally"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+                assert(checker.checkString('try {\n\ty++;\n} catch(e) {\n} finally\n{\n\tq = 5;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"while" loops', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['while'] });
+            });
+
+            it('should report extra newlines when configured with "while"', function() {
+                assert(checker.checkString('while (x < 10)\n{\n\tx++;\n}')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "while" and newline not added', function() {
+                assert(checker.checkString('while (x < 10) {\n\tx++;\n}')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "while"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+                assert(checker.checkString('while (x < 10)\n{\n\tx++;\n}')
+                    .isEmpty());
+            });
+        });
+
+        describe('"do...while" loops', function() {
+            beforeEach(function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['do'] });
+            });
+
+            it('should report extra newlines when configured with "do"', function() {
+                assert(checker.checkString('do\n{\n\tx++;\n} while (x < 10);')
+                    .getErrorCount() === 1);
+            });
+
+            it('should not complain when configured with "do" and newline not added', function() {
+                assert(checker.checkString('do {\n\tx++;\n} while (x < 10);')
+                    .isEmpty());
+            });
+
+            it('should not complain when not configured with "do"', function() {
+                checker.configure({ disallowNewlineBeforeBlockStatements: ['for'] });
+                assert(checker.checkString('do\n{\n\tx++;\n} while (x < 10);')
+                    .isEmpty());
+            });
+        });
+
+        describe('other block types', function() {
+            beforeEach(function() {
+                checker.configure({
+                    disallowNewlineBeforeBlockStatements: [
+                        'if',
+                        'else',
+                        'try',
+                        'catch',
+                        'finally',
+                        'do',
+                        'while',
+                        'for',
+                        'function'
+                    ]
+                });
+            });
+
+            it('should be ignored', function() {
+                assert(checker.checkString('{\nvar y = { "things": "stuff" };\n}')
+                    .isEmpty());
+            });
         });
     });
 });
