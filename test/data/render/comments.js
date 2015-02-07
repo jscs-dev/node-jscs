845a6b2a8d78e25c038fcb981c0334fe6f9c2d30
mdevils <mdevils@yandex.ru>
2015-02-07 17:53:49 +0300

    Autofixing: token data

---
+++ test/data/render/comments.js
@@ -0,0 +1,10 @@
+// Hello world!
+
+/* A little block comment */
+
+console.log('Hello World');
+
+// The rest of this file is an array of empty lines
+
+
+
---
+++ test/data/render/ios-includes.js
@@ -0,0 +1,10 @@
+
+#! random stuff
+#! 1234
+var a = 5;
+
+/* comment */
+
+#import "abc.js"
+#import abc.js
+var a = 5;
