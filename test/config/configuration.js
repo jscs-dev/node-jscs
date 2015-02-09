6a9ad23566e1342a131885ab7e9e053da6a39c57
Mike Sherov <mike.sherov@gmail.com>
2015-02-09 10:15:33 -0500

    Revert "Preset: JSCS"
    This reverts commit b336c659b4fd4fa35ed47306db7e7f29bbe65b4f.
    Patch backed outso that a patch release could be made.
    
    Refs ch-1015

--- test/config/configuration.js
+++ test/config/configuration.js
@@ -211,7 +211,6 @@ describe('modules/config/configuration', function() {
             assert(configuration.hasPreset('wikimedia'));
             assert(configuration.hasPreset('yandex'));
             assert(configuration.hasPreset('grunt'));
-            assert(configuration.hasPreset('jscs'));
         });
     });
 
