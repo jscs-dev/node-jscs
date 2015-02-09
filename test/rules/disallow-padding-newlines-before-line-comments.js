0c87477a53bbee4f54eab8a077f8f57978b5916e
Eli White <github@eli-white.com>
2015-02-09 01:26:42 -0800

    (disallow/require)PaddingNewLinesBeforeLineComments
    fixes: #598

---
+++ test/rules/disallow-padding-newlines-before-line-comments.js
@@ -0,0 +1,40 @@
+var Checker = require('../../lib/checker');
+var assert = require('assert');
+
+describe('rules/disallow-padding-newlines-before-line-comments', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+        checker.configure({ disallowPaddingNewLinesBeforeLineComments: true });
+    });
+
+    it('should report line comment after block comment with padding', function() {
+        assert(checker.checkString('var a = 2;\n/* comment */\n\n// comment').getErrorCount() === 1);
+    });
+
+    it('should report padding before line comment', function() {
+        assert(checker.checkString('var a = 2;\n\n// comment').getErrorCount() === 1);
+    });
+
+    it('should report additional padding before line comment', function() {
+        assert(checker.checkString('var a = 2;\n\n\n// comment').getErrorCount() === 1);
+    });
+
+    it('should not report missing padding before line comment', function() {
+        assert(checker.checkString('var a = 2;\n// comment').isEmpty());
+    });
+
+    it('should not report line comment after block comment', function() {
+        assert(checker.checkString('var a = 2;\n/* comment */\n// comment').isEmpty());
+    });
+
+    it('should not report missing padding if comment is first line', function() {
+        assert(checker.checkString('// comment\nvar a = 2;').isEmpty());
+    });
+
+    it('should not report missing padding with block comment', function() {
+        assert(checker.checkString('var a = 2;\n/* comment */').isEmpty());
+    });
+});
