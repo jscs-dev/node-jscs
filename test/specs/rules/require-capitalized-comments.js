74260921fdbbbb683e58178bf9b63d5b9c668ba2
Ash Clarke <ashclarke@users.noreply.github.com>
2015-02-12 16:07:23 +0000

    requireCapitalizedComments: Add `allExcept` option
    Fixes #1045
    Closes gh-1052

--- test/specs/rules/require-capitalized-comments.js
+++ test/specs/rules/require-capitalized-comments.js
@@ -11,91 +11,119 @@ describe('rules/require-capitalized-comments', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
-        checker.configure({ requireCapitalizedComments: true });
     });
 
-    it('should report on a lowercase start of a comment', function() {
-        assert(checker.checkString('//invalid').getErrorCount() === 1);
-        assert(checker.checkString('// invalid').getErrorCount() === 1);
-        assert(checker.checkString('/** invalid */').getErrorCount() === 1);
-        assert(checker.checkString('/* invalid */').getErrorCount() === 1);
-        assert(checker.checkString('/**\n * invalid\n*/').getErrorCount() === 1);
-        assert(checker.checkString('//\n// invalid\n//').getErrorCount() === 1);
-        assert(checker.checkString('/*\ninvalid\n*/').getErrorCount() === 1);
-        assert(checker.checkString('//\xFCber').getErrorCount() === 1);
-        assert(checker.checkString('//\u03C0').getErrorCount() === 1);
+    describe('option value true', function() {
+        beforeEach(function() {
+            checker.configure({ requireCapitalizedComments: true });
+        });
+
+        it('should report on a lowercase start of a comment', function() {
+            assert(checker.checkString('//invalid').getErrorCount() === 1);
+            assert(checker.checkString('// invalid').getErrorCount() === 1);
+            assert(checker.checkString('/** invalid */').getErrorCount() === 1);
+            assert(checker.checkString('/* invalid */').getErrorCount() === 1);
+            assert(checker.checkString('/**\n * invalid\n*/').getErrorCount() === 1);
+            assert(checker.checkString('//\n// invalid\n//').getErrorCount() === 1);
+            assert(checker.checkString('/*\ninvalid\n*/').getErrorCount() === 1);
+            assert(checker.checkString('//\xFCber').getErrorCount() === 1);
+            assert(checker.checkString('//\u03C0').getErrorCount() === 1);
+        });
+
+        it('should not report an uppercase start of a comment', function() {
+            assertEmpty('//Valid');
+            assertEmpty('// Valid');
+            assertEmpty('/** Valid */');
+            assertEmpty('//\n// Valid\n//');
+            assertEmpty('/*\nValid\n*/');
+            assertEmpty('//\xDCber');
+            assertEmpty('//\u03A0');
+        });
+
+        it('should not report on comments that start with a non-alphabetical character', function() {
+            assert(checker.checkString('//123').isEmpty());
+            assert(checker.checkString('// 123').isEmpty());
+            assert(checker.checkString('/**123*/').isEmpty());
+            assert(checker.checkString('/**\n @todo: foobar\n */').isEmpty());
+            assert(checker.checkString('/** 123*/').isEmpty());
+            assert(checker.checkString([
+                '// \xDCber',
+                '// diese Funktion'
+            ].join('\n')).isEmpty());
+        });
+
+        it('should not report on multiple uppercase lines in a "textblock"', function() {
+            assertEmpty([
+                '// This is a textblock.',
+                '// That has two uppercase lines'
+            ].join('\n'));
+
+            assertEmpty([
+                '// A textblock may also have multiple lines.',
+                '// Those lines can be uppercase as well to support',
+                '// sentense breaks in textblocks'
+            ].join('\n'));
+        });
+
+        it('should not report on multiple uppercase lines in multiple "textblocks"', function() {
+            assertEmpty([
+                '// This is a textblock',
+                '//',
+                '// That has two uppercase lines'
+            ].join('\n'));
+        });
+
+        it('should not report on multiple lines that start with non-letters', function() {
+            assertEmpty([
+                '// 123 or any non-alphabetical starting character',
+                '// @are also valid anywhere'
+            ].join('\n'));
+        });
+
+        it('should not report on a lowercase start of a comment in a "textblock"', function() {
+            assertEmpty([
+                '// This is a comment',
+                '// that is part of a textblock'
+            ].join('\n'));
+
+            assertEmpty([
+                '// This is a comment',
+                '// that is part of a textblock',
+                '//',
+                '// So is this the beginning',
+                '// of a textblock that can be',
+                '// entered in and out of'
+            ].join('\n'));
+
+            assertEmpty([
+                '// A textblock is a set of lines',
+                '// that starts with a capitalized letter',
+                '// and has one or more non-capitalized lines',
+                '// afterwards'
+            ].join('\n'));
+        });
+
     });
 
-    it('should not report an uppercase start of a comment', function() {
-        assertEmpty('//Valid');
-        assertEmpty('// Valid');
-        assertEmpty('/** Valid */');
-        assertEmpty('//\n// Valid\n//');
-        assertEmpty('/*\nValid\n*/');
-        assertEmpty('//\xDCber');
-        assertEmpty('//\u03A0');
-    });
+    describe('option value allExcept', function() {
+        beforeEach(function() {
+            checker.configure({ requireCapitalizedComments: { allExcept: ['istanbul', 'zombiecheckjs'] } });
+        });
 
-    it('should not report on comments that start with a non-alphabetical character', function() {
-        assert(checker.checkString('//123').isEmpty());
-        assert(checker.checkString('// 123').isEmpty());
-        assert(checker.checkString('/**123*/').isEmpty());
-        assert(checker.checkString('/**\n @todo: foobar\n */').isEmpty());
-        assert(checker.checkString('/** 123*/').isEmpty());
-        assert(checker.checkString([
-            '// \xDCber',
-            '// diese Funktion'
-        ].join('\n')).isEmpty());
-    });
+        it('should report for anything else', function() {
+            assert(checker.checkString('/* my comment: this is cool */').getErrorCount() === 1);
+        });
 
-    it('should not report on multiple uppercase lines in a "textblock"', function() {
-        assertEmpty([
-            '// This is a textblock.',
-            '// That has two uppercase lines'
-        ].join('\n'));
+        it('should report for other comment directives', function() {
+            assert(checker.checkString('/* jshint: -W071 */').getErrorCount() === 1);
 
-        assertEmpty([
-            '// A textblock may also have multiple lines.',
-            '// Those lines can be uppercase as well to support',
-            '// sentense breaks in textblocks'
-        ].join('\n'));
-    });
+            assert(checker.checkString('/* jscs:disable requireCurlyBraces */').getErrorCount() === 1);
+        });
 
-    it('should not report on multiple uppercase lines in multiple "textblocks"', function() {
-        assertEmpty([
-            '// This is a textblock',
-            '//',
-            '// That has two uppercase lines'
-        ].join('\n'));
-    });
+        it('should not report for custom exceptions', function() {
+            assertEmpty('/* istanbul ignore next */');
 
-    it('should not report on multiple lines that start with non-letters', function() {
-        assertEmpty([
-            '// 123 or any non-alphabetical starting character',
-            '// @are also valid anywhere'
-        ].join('\n'));
-    });
-
-    it('should not report on a lowercase start of a comment in a "textblock"', function() {
-        assertEmpty([
-            '// This is a comment',
-            '// that is part of a textblock'
-        ].join('\n'));
-
-        assertEmpty([
-            '// This is a comment',
-            '// that is part of a textblock',
-            '//',
-            '// So is this the beginning',
-            '// of a textblock that can be',
-            '// entered in and out of'
-        ].join('\n'));
-
-        assertEmpty([
-            '// A textblock is a set of lines',
-            '// that starts with a capitalized letter',
-            '// and has one or more non-capitalized lines',
-            '// afterwards'
-        ].join('\n'));
+            assertEmpty('/* zombiecheckjs: ensurebrains */');
+        });
     });
 });
