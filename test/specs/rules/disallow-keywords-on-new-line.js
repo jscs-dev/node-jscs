8cc2ba0b33175132ca83b3aa3d95be385805eadc
oredi <darren_seet@oredi.com>
2015-02-14 03:47:42 +0900

    disallowKeywordsOnNewLine: "do..while" on new line
    Make option "disallowKeywordsOnNewLine" perform special scenarios
    for the keyword "while" when checking "DoWhileStatements".
    
    The behavior contradicts the original idea of keyword "while" but it is done to
    reuse existing code in a efficient and less complex manner.
    
    1. Add getNodesByFirstToken to js-file that is used by
       disallow-keywords-on-new-line to handle "while" and "do-while"
    2. Allow "while" that is part of a "do while" with no braces to succeed
    3. Allow "while" that is not part of a "do while" to succeed even
       though it contradicts rule meaning
    
    Fixes #885
    Closes gh-1063

--- test/specs/rules/disallow-keywords-on-new-line.js
+++ test/specs/rules/disallow-keywords-on-new-line.js
@@ -42,6 +42,31 @@ describe('rules/disallow-keywords-on-new-line', function() {
                 '}'
     });
 
+    describe('illegal keyword placement for "do while" (#885)', function() {
+        beforeEach(function() {
+            checker.configure({ disallowKeywordsOnNewLine: ['while'] });
+
+            input = 'do {\n' +
+                    'x++;\n' +
+                '}\n' +
+                'while(x > 0)';
+
+            output = 'do {\n' +
+                    'x++;\n' +
+                '} while(x > 0)';
+        });
+
+        it('should report', function() {
+            assert(checker.checkString(input).getErrorCount() === 1);
+        });
+
+        it('should fix', function() {
+            var result = checker.fixString(input);
+            assert(result.errors.isEmpty());
+            assert.equal(result.output, output);
+        });
+    });
+
     it('should not report legal keyword placement', function() {
         checker.configure({ disallowKeywordsOnNewLine: ['else'] });
         assert(
@@ -61,6 +86,43 @@ describe('rules/disallow-keywords-on-new-line', function() {
             checker.checkString(
                 'if (block) block[v]["(type)"] = "var";\n' +
                 'else funct[v] = "var";'
+            ).isEmpty()
+        );
+    });
+
+    it('should not report legal keyword placement for "do while" (#885)', function() {
+        checker.configure({ disallowKeywordsOnNewLine: ['while'] });
+        assert(
+            checker.checkString(
+                'do {\n' +
+                    'x++;\n' +
+                '}while(x > 0)'
+            ).isEmpty()
+        );
+    });
+
+    it('should not report special case for "do while" multiple line statement without braces (#885)', function() {
+        checker.configure({ disallowKeywordsOnNewLine: ['while'] });
+        assert(
+            checker.checkString(
+                'do\n' +
+                    ';\n' +
+                'while(i > 0)'
+            ).isEmpty()
+        );
+    });
+
+    it('should not report legal keyword placement for "while" statement (#885)', function() {
+        checker.configure({ disallowKeywordsOnNewLine: ['while'] });
+        assert(
+            checker.checkString(
+                'var i = 0;\n' +
+                'if (i) {\n' +
+                    'i++;\n' +
+                '}' +
+                'while(i > 0) {\n' +
+                    ';\n' +
+                '}'
             ).isEmpty()
         );
     });
