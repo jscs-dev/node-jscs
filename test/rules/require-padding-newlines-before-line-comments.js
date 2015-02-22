afc670d59e5205345e4dfc9f25ca40faea0117eb
Eli White <github@eli-white.com>
2015-02-22 15:58:06 -0800

    requirePaddingNewLinesBeforeLineComments: Allow consecutive comments and firstAfterCurly exception
    Refs #1103
    Closes gh-1102

--- test/rules/require-padding-newlines-before-line-comments.js
+++ test/rules/require-padding-newlines-before-line-comments.js
@@ -7,34 +7,98 @@ describe('rules/require-padding-newlines-before-line-comments', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
-        checker.configure({ requirePaddingNewLinesBeforeLineComments: true });
     });
 
-    it('should report missing padding before line comment', function() {
-        assert(checker.checkString('var a = 2;\n// comment').getErrorCount() === 1);
+    describe('invalid options', function() {
+        it('should throw if false', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewLinesBeforeLineComments: false });
+            });
+        });
+
+        it('should throw if array', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewLinesBeforeLineComments: [] });
+            });
+        });
+
+        it('should throw if empty object', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewLinesBeforeLineComments: {} });
+            });
+        });
+
+        it('should throw if not allExcept object', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewLinesBeforeLineComments: { allBut: false} });
+            });
+        });
+
+        it('should throw if not allExcept firstAfterCurly', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewLinesBeforeLineComments: { allExcept: 'badOptionName'} });
+            });
+        });
     });
 
-    it('should report line comment after block comment', function() {
-        assert(checker.checkString('var a = 2;\n/* comment */\n// comment').getErrorCount() === 1);
+    describe('value true', function() {
+        beforeEach(function() {
+            checker.configure({ requirePaddingNewLinesBeforeLineComments: true });
+        });
+
+        it('should report missing padding before line comment', function() {
+            assert(checker.checkString('var a = 2;\n// comment').getErrorCount() === 1);
+        });
+
+        it('should report line comment after block comment', function() {
+            assert(checker.checkString('var a = 2;\n/* comment */\n// comment').getErrorCount() === 1);
+        });
+
+        it('should not report multiple line comments', function() {
+            assert(checker.checkString('// comment\n//foo').isEmpty());
+        });
+
+        it('should report one error if multiple comments dont have line space', function() {
+            assert(checker.checkString('var a = 2;\n// comment\n// comment').getErrorCount() === 1);
+        });
+
+        it('should not report missing padding if comment is first line', function() {
+            assert(checker.checkString('// comment\nvar a = 2;').isEmpty());
+        });
+
+        it('should not report padding before line comment', function() {
+            assert(checker.checkString('var a = 2;\n\n// comment').isEmpty());
+        });
+
+        it('should not report additional padding before line comment', function() {
+            assert(checker.checkString('var a = 2;\n\n\n// comment').isEmpty());
+        });
+
+        it('should not report missing padding with block comment', function() {
+            assert(checker.checkString('var a = 2;\n/* comment */').isEmpty());
+        });
+
+        it('should not report line comment after block comment with padding', function() {
+            assert(checker.checkString('var a = 2;\n/* comment */\n\n// comment').isEmpty());
+        });
+
+        it('should report error if first line after a curly', function() {
+            assert(checker.checkString('if (true) {\n// comment\n}').getErrorCount() === 1);
+        });
     });
 
-    it('should not report missing padding if comment is first line', function() {
-        assert(checker.checkString('// comment\nvar a = 2;').isEmpty());
-    });
+    describe('value allExcept: firstAfterCurly', function() {
+        beforeEach(function() {
+            checker.configure({
+                requirePaddingNewLinesBeforeLineComments: {
+                    allExcept: 'firstAfterCurly'
+                }
+            });
+        });
 
-    it('should not report padding before line comment', function() {
-        assert(checker.checkString('var a = 2;\n\n// comment').isEmpty());
-    });
-
-    it('should not report additional padding before line comment', function() {
-        assert(checker.checkString('var a = 2;\n\n\n// comment').isEmpty());
-    });
-
-    it('should not report missing padding with block comment', function() {
-        assert(checker.checkString('var a = 2;\n/* comment */').isEmpty());
-    });
-
-    it('should not report line comment after block comment with padding', function() {
-        assert(checker.checkString('var a = 2;\n/* comment */\n\n// comment').isEmpty());
+        it('should not report error if first line after a curly', function() {
+            assert(checker.checkString('if (true) {\n// comment\n}').isEmpty());
+            assert(checker.checkString('var a = {\n// comment\n};').isEmpty());
+        });
     });
 });
