b1dbf60fb659ff2e663ebe88f9520a6a82d47a6f
Evan Jacobs <evan@evoked.us>
2015-03-04 01:10:05 -0800

    New rule: requireEmptyLineAfterVariableDeclaration
    This check enforces a blank line of breathing room between `var` declarations
    and any non-`var` following content in the same block. This is conceptually
    similar to `requireLineBreakAfterVariableAssignment`, but enforces
    more aggressive minimum whitespace.
    
    Closes gh-1107
    Fixes #1107

---
+++ test/rules/require-empty-line-after-variable-declaration.js
@@ -0,0 +1,35 @@
+var Checker = require('../../lib/checker');
+var assert = require('assert');
+
+describe('rules/require-empty-line-after-variable-declaration', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+        checker.configure({ requireEmptyLineAfterVariableDeclaration: true });
+    });
+
+    it('should not report if an extra newline is present after var declaration', function() {
+        assert(checker.checkString('var a = 1;\n\nconsole.log(a);').isEmpty());
+        assert(checker.checkString('function a() { var a = 1;\n\nconsole.log(a); }').isEmpty());
+        assert(checker.checkString('var b = 2;\n\nfunction a() { var a = 1;\n\nconsole.log(a); }').isEmpty());
+        assert(checker.checkString(
+            'var b = 2;\n\nfunction a() { var a = 1;\n\nconsole.log(a); } var c = 3;'
+        ).isEmpty());
+    });
+
+    it('should not report for consecutive var declarations', function() {
+        assert(checker.checkString('var a = 1; var b = 2; var c = 3;').isEmpty());
+    });
+
+    it('should not report if var is the last expression in the block', function() {
+        assert(checker.checkString('function a() { var x; }').isEmpty());
+    });
+
+    it('should report if no extra newline is present after var declaration', function() {
+        assert(checker.checkString('var x; console.log(x);').getErrorCount() === 1);
+        assert(checker.checkString('function a() { var x; console.log(x); }').getErrorCount() === 1);
+        assert(checker.checkString('var y; function a() { var x; console.log(x); }').getErrorCount() === 2);
+    });
+});
