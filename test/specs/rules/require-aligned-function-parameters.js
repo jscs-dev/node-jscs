862bf049b4c7356b3d761fac0c0c7489e7f5038b
Bram Cordie <bramcordie@gmail.com>
2015-03-31 22:11:10 +0200

    New Rule: requireAlignedFunctionParameters
    fixed #1164

---
+++ test/specs/rules/require-aligned-function-parameters.js
@@ -0,0 +1,103 @@
+var Checker = require('../../../lib/checker');
+var assert = require('assert');
+
+describe('rules/require-aligned-function-parameters', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+    });
+
+    describe('no option', function() {
+        it('should not report inline function parameters', function() {
+            checker.configure({ requireAlignedFunctionParameters: true });
+            assert(
+                checker.checkString(
+                    'function a(b, c) {}'
+                ).isEmpty()
+            );
+        });
+
+        it('should report unaligned multi-line function parameters', function() {
+            checker.configure({ requireAlignedFunctionParameters: true });
+            assert(
+                checker.checkString(
+                    'function a(\n' +
+                        '  b,\n' +
+                        'c\n' +
+                    ') {}'
+                ).getErrorCount() === 1
+            );
+        });
+
+        it('should not report aligned multi-line function parameters', function() {
+            checker.configure({ requireAlignedFunctionParameters: true });
+            assert(
+                checker.checkString(
+                    'function a(b,\n' +
+                    '           c\) {}'
+                ).isEmpty()
+            );
+        });
+    });
+
+    describe('lineBreakAfterOpeningBrace', function() {
+        beforeEach(function() {
+            checker.configure({
+                requireAlignedFunctionParameters: {
+                    lineBreakAfterOpeningBrace: true
+                }
+            });
+        });
+
+        it('should not report a missing line break after the opening brace', function() {
+            assert(
+                checker.checkString(
+                    'function a(\n' +
+                        '  b,\n' +
+                        '  c) {}'
+                ).isEmpty()
+            );
+        });
+
+        it('should report a missing line break after the opening brace', function() {
+            assert(
+                checker.checkString(
+                    'function a(b,\n' +
+                    '           c) {}'
+                ).getErrorCount() === 1
+            );
+        });
+    });
+
+    describe('lineBreakBeforeClosingBrace', function() {
+        beforeEach(function() {
+            checker.configure({
+                requireAlignedFunctionParameters: {
+                    lineBreakBeforeClosingBrace: true
+                }
+            });
+        });
+
+        it('should not report a missing line break before the closing brace', function() {
+            assert(
+                checker.checkString(
+                    'function a(\n' +
+                        '  b,\n' +
+                        '  c\n' +
+                    ') {}'
+                ).isEmpty()
+            );
+        });
+
+        it('should report a missing line break before the closing brace', function() {
+            assert(
+                checker.checkString(
+                    'function a(b,\n' +
+                    '           c)\n' + ' {}'
+                ).getErrorCount() === 1
+            );
+        });
+    });
+});
