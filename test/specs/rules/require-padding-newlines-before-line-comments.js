36e627badba73eefb4a1dfaf221b752f1dc03756
Eli White <github@eli-white.com>
2015-04-13 11:22:18 -0700

    requirePaddingNewLinesBeforeLineComments: first token and not first line
    Fix for file starting with line comment that isn't the first line
    
    Fixes #1261
    Closes gh-1262

--- test/specs/rules/require-padding-newlines-before-line-comments.js
+++ test/specs/rules/require-padding-newlines-before-line-comments.js
@@ -66,6 +66,10 @@ describe('rules/require-padding-newlines-before-line-comments', function() {
             assert(checker.checkString('// comment\nvar a = 2;').isEmpty());
         });
 
+        it('should not report missing padding if comment is first token and not first line (#1261)', function() {
+            assert(checker.checkString('\n// comment').isEmpty());
+        });
+
         it('should not report padding before line comment', function() {
             assert(checker.checkString('var a = 2;\n\n// comment').isEmpty());
         });
@@ -84,6 +88,14 @@ describe('rules/require-padding-newlines-before-line-comments', function() {
 
         it('should report error if first line after a curly', function() {
             assert(checker.checkString('if (true) {\n// comment\n}').getErrorCount() === 1);
+        });
+
+        it('should report error if first line after shebang', function() {
+            assert(checker.checkString('#!/usr/bin/env node\n// comment').getErrorCount() === 1);
+        });
+
+        it('should not report if padding after shebang', function() {
+            assert(checker.checkString('#!/usr/bin/env node\n\n// comment').isEmpty());
         });
     });
 
