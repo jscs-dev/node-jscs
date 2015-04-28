fccdf2b0b278ed8bb0c561122a0e91cc3c80291a
Stefano Sala <stefano.sala@spa.it>
2015-04-28 16:02:19 +0200

    disallowMultipleVarDecl: add exception for require statements
    This add a `allExcept[require]` option for `disallowMultipleVarDecl` to allow multiple var declarations only when requiring external lib, tipically at the beginning of a nodejs module.

--- test/specs/rules/disallow-multiple-var-decl.js
+++ test/specs/rules/disallow-multiple-var-decl.js
@@ -79,4 +79,52 @@ describe('rules/disallow-multiple-var-decl', function() {
             assert(checker.checkString('var x, y = 2, z;').getErrorCount() === 1);
         });
     });
+
+    describe('exceptRequire', function() {
+        beforeEach(function() {
+            checker.configure({ disallowMultipleVarDecl: { allExcept: ['require'] } });
+        });
+        it('should not report multiple var decls with require', function() {
+            assert(checker.checkString('var first = require("first"), second = require("second");').isEmpty());
+        });
+        it('should report multiple var decls with require mixed with normal', function() {
+            assert.equal(1, checker.checkString('var first = require("first"), second = 1;').getErrorCount());
+        });
+        it('should report multiple var decls', function() {
+            assert.equal(1, checker.checkString('var x, y;').getErrorCount());
+        });
+        it('should not report consecutive var decls', function() {
+            assert(checker.checkString('var x; var y;').isEmpty());
+        });
+    });
+
+    describe('options as object', function() {
+        it('should accept undefined as allExcept value', function() {
+            checker.configure({ disallowMultipleVarDecl: { allExcept: ['undefined'] } });
+
+            assert(checker.checkString('var x, y;').isEmpty());
+        });
+        it('should accept require and undefined as allExcept value', function() {
+            checker.configure({ disallowMultipleVarDecl: { allExcept: ['undefined', 'require'] } });
+
+            assert(checker.checkString('var a = require("a"), b = require("b"), x, y, z;').isEmpty());
+            assert.equal(
+                1,
+                checker.checkString('var a = require("a"), b = require("b"), x, y, c = 1;').getErrorCount()
+            );
+        });
+        it('should accept strict as option', function() {
+            checker.configure({ disallowMultipleVarDecl: { strict: true } });
+
+            assert(!checker.checkString('for (var i = 0, j = arr.length; i < j; i++) {}').isEmpty());
+        });
+        it('should accept all options at the same time', function() {
+            checker.configure({ disallowMultipleVarDecl: { strict: true, allExcept: ['undefined', 'require'] } });
+
+            assert(!checker.checkString(
+                'var a = require("a"), b = require("b"), x, y;' +
+                'for (var i = 0, j = arr.length; i < j; i++) {}'
+            ).isEmpty());
+        });
+    });
 });
