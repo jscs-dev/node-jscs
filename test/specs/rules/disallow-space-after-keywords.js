3a9a156d5fb577244dab6ac78c8507e18918ea6f
Oleg Gaidarenko <markelog@gmail.com>
2015-05-11 03:50:42 +0300

    Tests: correct couple assertions

--- test/specs/rules/disallow-space-after-keywords.js
+++ test/specs/rules/disallow-space-after-keywords.js
@@ -19,20 +19,19 @@ describe('rules/disallow-space-after-keywords', function() {
         assert(checker.checkString('if(x) { x++; }').isEmpty());
     });
 
-    it('should report on all spaced keywords if a value of true is supplied', function() {
+    it('should report on all spaced keywords if a value true is supplied', function() {
         checker.configure({ disallowSpaceAfterKeywords: true });
 
-        assert(!checker.checkString('do {}').isEmpty());
-        assert(!checker.checkString('for (){}').isEmpty());
+        assert(!checker.checkString('do {} while (false)').isEmpty());
+        assert(!checker.checkString('for (;;){}').isEmpty());
         assert(!checker.checkString('if (x) {}').isEmpty());
-        assert(!checker.checkString('if (){} else {}').isEmpty());
-        assert(!checker.checkString('switch (){ case 4: break;}').isEmpty());
-        assert(!checker.checkString('switch(){ case \'4\': break;}').isEmpty());
-        assert(!checker.checkString('try {}').isEmpty());
-        assert(!checker.checkString('try{} catch {}').isEmpty());
+        assert(!checker.checkString('if (true){} else{}').isEmpty());
+        assert(!checker.checkString('switch (false){ case 4: break;}').isEmpty());
+        assert(!checker.checkString('switch(true){ case \'4\': break;}').isEmpty());
+        assert(!checker.checkString('try {} catch(e) {}').isEmpty());
         assert(!checker.checkString('void (0)').isEmpty());
         assert(!checker.checkString('while (x) {}').isEmpty());
-        assert(!checker.checkString('with (){}').isEmpty());
+        assert(!checker.checkString('with ({}){}').isEmpty());
         assert(!checker.checkString('function foo(){}').isEmpty());
         assert(!checker.checkString('typeof \'4\'').isEmpty());
     });
