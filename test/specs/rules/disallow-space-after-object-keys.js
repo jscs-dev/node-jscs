903c389390f04860ee9c71591a2f0345a91dd965
Henry Zhu <hi@henryzoo.com>
2015-05-11 18:14:34 -0400

    (disallow|require)SpaceAfterObjectKeys: check object method shorthand
    Fixes #1365
    Closes gh-1366

--- test/specs/rules/disallow-space-after-object-keys.js
+++ test/specs/rules/disallow-space-after-object-keys.js
@@ -81,13 +81,24 @@ describe('rules/disallow-space-after-object-keys', function() {
         });
     });
 
-    it('should not report es6-methods. #1013', function() {
-        checker.configure({ esnext: true });
-        assert(checker.checkString('var x = { a() { } };').isEmpty());
-    });
-
     it('should not report es5 getters/setters #1037', function() {
+        checker.configure({ disallowSpaceAfterObjectKeys: true });
         assert(checker.checkString('var x = { get a() { } };').isEmpty());
         assert(checker.checkString('var x = { set a(val) { } };').isEmpty());
+    });
+
+    describe('es6', function() {
+        beforeEach(function() {
+            checker.configure({ esnext: true, disallowSpaceAfterObjectKeys: true });
+        });
+
+        it('should not report es6-methods without a space. #1013', function() {
+            assert(checker.checkString('var x = { a() { } };').isEmpty());
+        });
+
+        it('should report es6-methods with a space. #1013', function() {
+            assert(checker.checkString('var x = { a () { } };').getErrorCount() === 1);
+        });
+
     });
 });
