cd8ce2e36b4c7fece12ad33cc7e7709ff71fd561
Andrey Ermakov <dreef3@gmail.com>
2015-05-29 02:54:42 +0300

    disallowCommaBeforeLineBreak: added allExcept function
    Added supoort to ignore object expressions with function properties
    and to place a comma on the new line when fixing.
    
    "lineBreak" option changed into default behavior for multiline expressions
    
    Closes gh-1410

--- test/specs/rules/disallow-comma-before-line-break.js
+++ test/specs/rules/disallow-comma-before-line-break.js
@@ -15,22 +15,25 @@ describe('rules/disallow-comma-before-line-break', function() {
     reportAndFix({
         name: 'illegal comma placement in multiline var declaration',
         rules: rules,
+        errors: 2,
         input: 'var a,\nb;',
-        output: 'var a, b;'
+        output: 'var a\n, b;'
     });
 
     reportAndFix({
         name: 'illegal comma placement in multiline array declaration',
         rules: rules,
+        errors: 2,
         input: 'var a = [1,\n2];',
-        output: 'var a = [1, 2];'
+        output: 'var a = [1\n, 2];'
     });
 
     reportAndFix({
         name: 'illegal comma placement in multiline object declaration',
         rules: rules,
+        errors: 2,
         input: 'var a = {a:1,\nc:3};',
-        output: 'var a = {a:1, c:3};'
+        output: 'var a = {a:1\n, c:3};'
     });
 
     it('should not report legal comma placement in multiline var declaration', function() {
@@ -59,5 +62,33 @@ describe('rules/disallow-comma-before-line-break', function() {
 
     it('should not report comma placement in single line object declaration', function() {
         assert(checker.checkString('var a = {a:1, c:3};').isEmpty());
+    });
+
+    describe('options as object', function() {
+        describe('allExcept as option', function() {
+            describe('with value `function`', function() {
+                beforeEach(function() {
+                    checker = new Checker();
+                    checker.registerDefaultRules();
+                    checker.configure({disallowCommaBeforeLineBreak: {allExcept: ['function']}});
+                });
+                it('should not report objects with function values', function() {
+                    assert(
+                        checker.checkString(
+                            'var x = {\n' +
+                                'a : 1,\n' +
+                                'foo : function() {},\n' +
+                                'bcd : 2\n' +
+                            '};'
+                        ).isEmpty()
+                    );
+                });
+                it('should report objects without function values', function() {
+                    assert(
+                        checker.checkString('var a = {a:1,\nc:3};').getErrorCount() === 2
+                    );
+                });
+            });
+        });
     });
 });
