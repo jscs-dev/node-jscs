974bd04520fe87da796e0a7f4cada0df7bef634b
Henry Zhu <hi@henryzoo.com>
2015-05-31 20:29:08 -0400

    requireTemplateStrings: refactor, check if either node is a string
    Fixes #1416
    Closes gh-1422

--- test/specs/rules/require-template-strings.js
+++ test/specs/rules/require-template-strings.js
@@ -18,18 +18,6 @@ describe('rules/require-template-strings', function() {
         assert(checker.checkString('"a" + a').getErrorCount() === 1);
     });
 
-    it('should not report the use of string concatenation with a identifier on the left and right', function() {
-        assert(checker.checkString('a + a').isEmpty());
-    });
-
-    it('should not report the use of string concatenation with two literals', function() {
-        assert(checker.checkString('"a" + "a"').isEmpty());
-    });
-
-    it('should not report the use of template strings', function() {
-        assert(checker.checkString('`How are you, ${name}?`').isEmpty());
-    });
-
     it('should report the use of string concatenation with right handed binary expression',
         function() {
             assert(checker.checkString('"test" + (a + b)').getErrorCount() === 1);
@@ -41,6 +29,46 @@ describe('rules/require-template-strings', function() {
             assert(checker.checkString('(a + b) + "test"').getErrorCount() === 1);
         }
     );
+
+    it('should report for the use of string concatenation with a CallExpression',
+        function() {
+            assert(checker.checkString('a() + "a"').getErrorCount() === 1);
+            assert(checker.checkString('"a" + a()').getErrorCount() === 1);
+        }
+    );
+
+    it('should report for the use of string concatenation with a MemberExpression',
+        function() {
+            assert(checker.checkString('a.b + "a"').getErrorCount() === 1);
+            assert(checker.checkString('"a" + a.b').getErrorCount() === 1);
+        }
+    );
+
+    it('should report for the use of string concatenation with a TemplateLiteral',
+        function() {
+            assert(checker.checkString('`a` + "a"').getErrorCount() === 1);
+            assert(checker.checkString('"a" + `a`').getErrorCount() === 1);
+        }
+    );
+
+    it('should report for the use of string concatenation with a TaggedTemplateExpression',
+        function() {
+            assert(checker.checkString('a`a` + "a"').getErrorCount() === 1);
+            assert(checker.checkString('"a" + a`a`').getErrorCount() === 1);
+        }
+    );
+
+    it('should report the use of string concatenation with two string literals', function() {
+        assert(checker.checkString('"a" + "a"').getErrorCount() === 1);
+    });
+
+    it('should not report the use of string concatenation with a identifier on the left and right', function() {
+        assert(checker.checkString('a + a').isEmpty());
+    });
+
+    it('should not report the use of template strings', function() {
+        assert(checker.checkString('`How are you, ${name}?`').isEmpty());
+    });
 
     it('should not report for number literals', function() {
         assert(checker.checkString('1 + a').isEmpty());
