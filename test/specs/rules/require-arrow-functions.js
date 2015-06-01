3926df09a4facb8bfdb027d9bc03cd4d7a3cadf4
Henry Zhu <hi@henryzoo.com>
2015-05-31 20:54:11 -0400

    requireArrowFunctions: don't check AssignmentExpression or Property
    Fixes #1413

--- test/specs/rules/require-arrow-functions.js
+++ test/specs/rules/require-arrow-functions.js
@@ -10,7 +10,7 @@ describe('rules/require-arrow-functions', function() {
         checker.configure({ requireArrowFunctions: true, esnext: true });
     });
 
-    it('should report use of anonymous function expression', function() {
+    it('should report use of anonymous function expression in VariableDeclaration', function() {
         assert(checker.checkString([
             'var anon = function(n) {',
                 'return n + 1;',
@@ -18,7 +18,7 @@ describe('rules/require-arrow-functions', function() {
         ].join('\n')).getErrorCount() === 1);
     });
 
-    it('should report use of named function expression', function() {
+    it('should report use of named function expression in VariableDeclaration', function() {
         assert(checker.checkString([
             'var a = function named(n) {',
                 'return n + 1;',
@@ -34,16 +34,25 @@ describe('rules/require-arrow-functions', function() {
         ].join('\n')).getErrorCount() === 1);
     });
 
-    it('should report use of object property function expression', function() {
+    it('should report use of function expression in a ReturnStatement', function() {
+        assert(checker.checkString('function a() { return function() {} }').getErrorCount() === 1);
+    });
+
+    it('should not report use of object property function expression #1413', function() {
         assert(checker.checkString([
             'var foo = {};',
             'foo.bar = function() {};'
-        ].join('\n')).getErrorCount() === 1);
+        ].join('\n')).isEmpty());
         assert(checker.checkString([
             'var foo = {',
               'bar: function() {}',
             '};'
-        ].join('\n')).getErrorCount() === 1);
+        ].join('\n')).isEmpty());
+    });
+
+    it('should not report function expression in a AssignmentExpression', function() {
+        assert(checker.checkString('a.b = function() {}').isEmpty());
+        assert(checker.checkString('a.b.c = function() {}').isEmpty());
     });
 
     it('should not report a function declaration', function() {
