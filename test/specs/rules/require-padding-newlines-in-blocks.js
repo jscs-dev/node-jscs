46a69dab89ef359074d96e695eb1fec95fd5f3ae
oredi <darren_seet@oredi.com>
2015-06-22 16:22:47 +0900

    requirePaddingNewlinesInBlocks: Refactor unit tests
    Change the unit tests to apply to all options except in special cases.
    Reuse the same test cases for all options by making use of Mocha forEach statements
    
    Fixes #1485
    Closes gh-1477

--- test/specs/rules/require-padding-newlines-in-blocks.js
+++ test/specs/rules/require-padding-newlines-in-blocks.js
@@ -8,114 +8,178 @@ describe('rules/require-padding-newlines-in-blocks', function() {
         checker.registerDefaultRules();
     });
 
-    describe('option value true', function() {
-        beforeEach(function() {
-            checker.configure({ requirePaddingNewlinesInBlocks: true });
+    var scenarios = [
+        { option: true, statement: 'abc();' },
+        { option: 1, statement: 'abc();abc();' }
+    ];
+
+    describe('invalid options', function() {
+        it('should report configuration error if not boolean or integer', function() {
+            assert.throws(function() {
+                checker.configure({ requirePaddingNewlinesInBlocks: 'string' });
+            });
         });
 
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {abc();\n\n}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {\nabc();\n\n}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newline before closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newline before closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();\n}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newlines in both cases', function() {
-            assert(checker.checkString('if (true) {abc();}').getErrorCount() === 2);
-        });
-
-        it('should report missing padding newlines in both cases', function() {
-            assert(checker.checkString('if (true) {\nabc();\n}').getErrorCount() === 2);
-        });
-
-        it('should not report with no spaces', function() {
-            assert(checker.checkString('if (true) {\n\nabc();\n\n}').isEmpty());
-        });
-
-        it('should not report empty function definitions', function() {
-            assert(checker.checkString('var a = function() {};').isEmpty());
-        });
-
-        it('should not report missing newlines after opening brace', function() {
-            assert(checker.checkString('if (true) {\n\n\nabc();\n\n}').isEmpty());
-        });
-
-        it('should not report missing newlines before closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();\nabc();\n\n\n}').isEmpty());
-        });
-
-        it('should not report missing newlines with comments inside block', function() {
-            assert(checker.checkString('if (true) {\n\n//bla\nabc();\nabc();\n\n}').isEmpty());
-        });
-
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {//bla\n\nabc();\n\n}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {\n/**/\nabc();\n\n}').getErrorCount() === 1);
-        });
-
-        it('should report missing padding newline between oneline comment and closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();\n\n//\n}').getErrorCount() === 1);
-        });
-
-        it('should fix missing padding newlines', function() {
-            assert.equal(
-                checker.fixString('if (true) {abc();abc();}').output,
-                'if (true) {\n\nabc();abc();\n\n}'
-            );
+        describe('option is boolean', function() {
+            it('should report configuration error if false', function() {
+                assert.throws(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: false });
+                });
+            });
         });
     });
 
-    describe('option value 1', function() {
-        beforeEach(function() {
-            checker.configure({ requirePaddingNewlinesInBlocks: 1 });
-        });
+    scenarios.forEach(function(scenario) {
+        describe('valid option: ' + JSON.stringify(scenario.option), function() {
+            it('should not report configuration error', function() {
+                assert.doesNotThrow(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
+            });
 
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {abc();abc();\n\n}').getErrorCount() === 1);
-        });
+            describe('tests for missing padding newline after opening brace', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
 
-        it('should report missing padding newline after opening brace', function() {
-            assert(checker.checkString('if (true) {\nabc();abc();\n\n}').getErrorCount() === 1);
-        });
+                it('should report error for no linebreak and no padding newline', function() {
+                    assert(checker.
+                        checkString('if (true) {' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
 
-        it('should report missing padding newline before closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();abc();}').getErrorCount() === 1);
-        });
+                it('should report error for linebreak but no padding newline', function() {
+                    assert(checker.
+                        checkString('if (true) {\n' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
 
-        it('should report missing padding newline before closing brace', function() {
-            assert(checker.checkString('if (true) {\n\nabc();abc();\n}').getErrorCount() === 1);
-        });
+                it('should report error for with no linebreak and no padding newline before single line comments',
+                    function() {
+                    assert(checker.
+                        checkString('if (true) {//bla\n' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
 
-        it('should report missing padding newlines in both cases', function() {
-            assert(checker.checkString('if (true) {abc();abc();}').getErrorCount() === 2);
-        });
+                it('should report error for linebreak but no padding newline before single line comments', function() {
+                    assert(checker.
+                        checkString('if (true) {\n//bla\n' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
 
-        it('should report missing padding newlines in both cases', function() {
-            assert(checker.checkString('if (true) {\nabc();abc();\n}').getErrorCount() === 2);
-        });
+                it('should report error for no linebreak and no padding newline before mulitiline comments',
+                    function() {
+                    assert(checker.
+                        checkString('if (true) {/**/\n' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
 
-        it('should not report with no spaces', function() {
-            assert(checker.checkString('if (true) {\n\nabc();abc();\n\n}').isEmpty());
-        });
+                it('should report error for linebreak but no padding newline before mulitiline comments', function() {
+                    assert(checker.
+                        checkString('if (true) {\n/**/\n' + scenario.statement + '\n\n}').getErrorCount() === 1);
+                });
+            });
 
-        it('should not report empty function definitions', function() {
-            assert(checker.checkString('var a = function() {};').isEmpty());
-        });
+            describe('tests for missing padding newline before closing brace', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
 
-        it('should not report single statement functions', function() {
-            assert(checker.checkString('var a = function() {abc();};').isEmpty());
+                it('should report error for no linebreak and no padding newline', function() {
+                    assert(checker
+                        .checkString('if (true) {\n\n' + scenario.statement + '}').getErrorCount() === 1);
+                });
+
+                it('should report error for with linebreak but no padding newline', function() {
+                    assert(checker
+                        .checkString('if (true) {\n\n' + scenario.statement + '\n}').getErrorCount() === 1);
+                });
+
+                it('should report error for no linebreak and no padding newline after single line comments',
+                    function() {
+                    assert(checker
+                        .checkString('if (true) {\n\n' + scenario.statement + '\n//bla}').getErrorCount() === 1);
+                });
+
+                it('should report error for linebreak but no padding newline after single line comments', function() {
+                    assert(checker.
+                        checkString('if (true) {\n\n' + scenario.statement + '\n//bla\n}').getErrorCount() === 1);
+                });
+
+                it('should report error for no linebreak and no padding newline after mulitiline comments', function() {
+                    assert(checker.
+                        checkString('if (true) {\n\n' + scenario.statement + '\n/**/}').getErrorCount() === 1);
+                });
+
+                it('should report error for linebreak but no padding newline after mulitiline comments', function() {
+                    assert(checker.
+                        checkString('if (true) {\n\n' + scenario.statement + '\n/**/\n}').getErrorCount() === 1);
+                });
+            });
+
+            describe('tests for missing padding newlines both after open brace and before closing brace', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
+
+                it('should report 2 errors for no linebreak and no padding newline', function() {
+                    assert(checker
+                        .checkString('if (true) {' + scenario.statement + '}').getErrorCount() === 2);
+                });
+
+                it('should report 2 errors for linebreak but no padding newline', function() {
+                    assert(checker
+                        .checkString('if (true) {\n' + scenario.statement + '\n}').getErrorCount() === 2);
+                });
+            });
+
+            describe('tests for valid padding newlines after opening brace and before closing brace', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
+
+                it('should not report error for single padding newline on both sides', function() {
+                    assert(checker.checkString('if (true) {\n\n' + scenario.statement + '\n\n}').isEmpty());
+                });
+
+                it('should not report error for double padding newline after opening brace and single padding ' +
+                    'newline before closing brace', function() {
+                    assert(checker.checkString('if (true) {\n\n\n' + scenario.statement + '\n\n}').isEmpty());
+                });
+
+                it('should not report error for single padding newline after opening brace and double padding  ' +
+                    'newline before closing brace', function() {
+                    assert(checker.checkString('if (true) {\n\n' + scenario.statement + '\n\n\n}').isEmpty());
+                });
+
+                it('should not report error for single padding newline on both sides with comments', function() {
+                    assert(checker.checkString('if (true) {\n\n//bla\n' + scenario.statement + '\n\n}').isEmpty());
+                });
+            });
+
+            describe('tests for special valid rule exceptions', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
+
+                it('should not report empty function definitions', function() {
+                    assert(checker.checkString('var a = function() {};').isEmpty());
+                });
+
+                if (typeof scenario.option === 'number' && scenario.option === 1) {
+                    it('should not report single statement functions', function() {
+                        assert(checker.checkString('var a = function() {abc();};').isEmpty());
+                    });
+                }
+            });
+
+            describe('tests for fixString', function() {
+                beforeEach(function() {
+                    checker.configure({ requirePaddingNewlinesInBlocks: scenario.option });
+                });
+
+                it('should fix missing padding newlines', function() {
+                    assert.equal(
+                        checker.fixString('if (true) {abc();abc();}').output,
+                        'if (true) {\n\nabc();abc();\n\n}'
+                    );
+                });
+            });
         });
     });
 });
