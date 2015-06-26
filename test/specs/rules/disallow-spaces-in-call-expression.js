31ec3a0a3f76233882f330bbb0dcd5c5da4ec6db
Inian Parameshwaran <inian1234@gmail.com>
2015-06-26 11:02:54 +0800

    disallowSpacesInCallExpression: Extend rule to validate NewExpression
    Fixes #898
    Closes gh-1490

--- test/specs/rules/disallow-spaces-in-call-expression.js
+++ test/specs/rules/disallow-spaces-in-call-expression.js
@@ -21,6 +21,17 @@ describe('rules/disallow-spaces-in-call-expression', function() {
         assert(checker.checkString('var x = function (){}();').isEmpty());
     });
 
+    it('should not report missing space before round brace in NewExpression', function() {
+        assert(checker.checkString('var x = new foobar();').isEmpty());
+        assert(checker.checkString('var x = new foo.bar();').isEmpty());
+        assert(checker.checkString('var x = new foo. bar();').isEmpty());
+        assert(checker.checkString('var x = new (foo .bar)();').isEmpty());
+        assert(checker.checkString('var x = new (function (){})();').isEmpty());
+        assert(checker.checkString('var x = new (function (){foobar();})();').isEmpty());
+        assert(checker.checkString('new (function(){ foobar(); })();').isEmpty());
+        assert(checker.checkString('var x = new function (){}();').isEmpty());
+    });
+
     it('should report space before round brace in CallExpression', function() {
         assert(checker.checkString('var x = foobar ();').getErrorCount() === 1);
         assert(checker.checkString('var x = foo.bar ();').getErrorCount() === 1);
@@ -30,5 +41,27 @@ describe('rules/disallow-spaces-in-call-expression', function() {
         assert(checker.checkString('var x = (function(){foobar ();}) ();').getErrorCount() === 2);
         assert(checker.checkString('(function(){foobar ();}) ();').getErrorCount() === 2);
         assert(checker.checkString('var x = function (){} ();').getErrorCount() === 1);
+    });
+
+    it('should report space before round brace in NewExpression', function() {
+        assert(checker.checkString('var x = new foobar ();').getErrorCount() === 1);
+        assert(checker.checkString('var x = new foo.bar ();').getErrorCount() === 1);
+        assert(checker.checkString('var x = new foo. bar ();').getErrorCount() === 1);
+        assert(checker.checkString('var x = new (foor .bar) ();').getErrorCount() === 1);
+        assert(checker.checkString('var x = new (function(){}) ();').getErrorCount() === 1);
+        assert(checker.checkString('var x = new (function(){new foobar ();}) ();').getErrorCount() === 2);
+        assert(checker.checkString('new (function(){new foobar ();}) ();').getErrorCount() === 2);
+        assert(checker.checkString('var x = new function (){} ();').getErrorCount() === 1);
+    });
+
+    it('should ignore NewExpression without parentheses', function() {
+        assert(checker.checkString('var x = new foobar ;').isEmpty());
+        assert(checker.checkString('var x = new foo.bar ;').isEmpty());
+        assert(checker.checkString('var x = new foo. bar ;').isEmpty());
+        assert(checker.checkString('var x = new (foor .bar) ;').isEmpty());
+        assert(checker.checkString('var x = new (function(){}) ;').isEmpty());
+        assert(checker.checkString('var x = new (function(){new foobar ;}) ;').isEmpty());
+        assert(checker.checkString('new (function(){new foobar ;}) ;').isEmpty());
+        assert(checker.checkString('var x = new function (){} ;').isEmpty());
     });
 });
