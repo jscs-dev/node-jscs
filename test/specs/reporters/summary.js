2417ff27e470c11e07309148187bd1ef6ce6c328
oredi <darren_seet@oredi.com>
2015-06-29 15:35:26 +0900

    Reporters: add new summary reporter
    This reporter provides a summary reports of:
    * All files with errors found listing each filename against the error count
    * All rules with errors found listing each rule with the error count and
      no of files with the rule error
    
    The following shows a sample of the output using cli-table:
        ┌───────┬─────────────┐
        │ Path  │ Error Count │
        ├───────┼─────────────┤
        │ input │ 1           │
        └───────┴─────────────┘
    
        ┌──────────────────┬──────────────┬───────────────────┐
        │ Rule             │ Total Errors │ Files With Errors │
        ├──────────────────┼──────────────┼───────────────────┤
        │ disallowKeywords │ 1            │ 1                 │
        ├──────────────────┼──────────────┼───────────────────┤
        │ All              │ 1            │ 1                 │
        └──────────────────┴──────────────┴───────────────────┘
    
    Fixes #1047
    Closes gh-1498

---
+++ test/specs/reporters/summary.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+var sinon = require('sinon');
+
+var Checker = require('../../../lib/checker');
+var summaryReporter = require('../../../lib/reporters/summary');
+
+describe('reporters/summary', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+        checker.configure({ disallowKeywords: ['with'] });
+
+        sinon.stub(console, 'log');
+    });
+
+    afterEach(function() {
+        console.log.restore();
+    });
+
+    it('should correctly reports no errors', function() {
+        summaryReporter([checker.checkString('a++;')]);
+        assert.equal(console.log.getCall(0).args[0], 'No code style errors found.');
+        assert(console.log.calledOnce);
+    });
+
+    it('should correctly report 1 error', function() {
+        var expectedByFile = /input.*1/;
+        var expectedByRule = /disallowKeywords.*1.*1/;
+        var expectedByRuleTotal = /All.*1.*1/;
+
+        summaryReporter([checker.checkString('with (x) {}')]);
+        assert(expectedByFile.test(console.log.getCall(0).args[0]));
+        assert(expectedByRule.test(console.log.getCall(1).args[0]));
+        assert(expectedByRuleTotal.test(console.log.getCall(1).args[0]));
+        assert.equal(console.log.callCount, 2);
+    });
+
+    it('should correctly report 2 errors', function() {
+        var expectedByFile = /input.*2/;
+        var expectedByRule = /disallowKeywords.*2.*1/;
+        var expectedByRuleTotal = /All.*2.*1/;
+
+        summaryReporter([checker.checkString('with(x){} with(x){} ')]);
+        assert(expectedByFile.test(console.log.getCall(0).args[0]));
+        assert(expectedByRule.test(console.log.getCall(1).args[0]));
+        assert(expectedByRuleTotal.test(console.log.getCall(1).args[0]));
+        assert.equal(console.log.callCount, 2);
+    });
+});
