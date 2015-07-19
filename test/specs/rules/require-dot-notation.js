b5437fe6b54438e86f4d0569039925aa8d54139d
Alexej Yaroshevich <alex@qfox.ru>
2015-07-19 09:49:13 +0300

    Misc: change default dialect to es5 and make appropriate changes
    BREAKING!
    
    - change default dialect to es5 (via disabling es3 flag)
    - rework requireDotNotation rule a little
    - update jquery and wordpress presets according to new value format
    
    Fixes #824

--- test/specs/rules/require-dot-notation.js
+++ test/specs/rules/require-dot-notation.js
@@ -16,16 +16,13 @@ describe('rules/require-dot-notation', function() {
 
         it('should report literal subscription', function() {
             assert.equal(checker.checkString('var x = a[\'b\']').getErrorCount(), 1);
+            assert.equal(checker.checkString('var x = a[\'while\']').getErrorCount(), 1);
         });
 
         it('should not report literal subscription for reserved words', function() {
-            assert(checker.checkString('var x = a[\'while\']').isEmpty());
             assert(checker.checkString('var x = a[null]').isEmpty());
             assert(checker.checkString('var x = a[true]').isEmpty());
             assert(checker.checkString('var x = a[false]').isEmpty());
-            assert(checker.checkString('var x = a["null"]').isEmpty());
-            assert(checker.checkString('var x = a["true"]').isEmpty());
-            assert(checker.checkString('var x = a["false"]').isEmpty());
         });
 
         it('should not report number subscription', function() {
@@ -53,7 +50,7 @@ describe('rules/require-dot-notation', function() {
         });
     });
 
-    describe('true value with es3 explicitly enabled', function() {
+    describe('true value with es3 enabled', function() {
         beforeEach(function() {
             checker.configure({ es3: true, requireDotNotation: true });
         });
@@ -64,20 +61,9 @@ describe('rules/require-dot-notation', function() {
         });
     });
 
-    describe('true value with es3 explicitly disabled', function() {
+    describe('option value {"allExcept":"snake_case"}', function() {
         beforeEach(function() {
-            checker.configure({ es3: false, requireDotNotation: true });
-        });
-
-        it('should not report literal subscription for es3 keywords or future reserved words', function() {
-            assert.equal(checker.checkString('var x = a[\'while\']').getErrorCount(), 1);
-            assert.equal(checker.checkString('var x = a[\'abstract\']').getErrorCount(), 1);
-        });
-    });
-
-    describe('option value `except_snake_case`', function() {
-        beforeEach(function() {
-            checker.configure({ requireDotNotation: 'except_snake_case' });
+            checker.configure({ requireDotNotation: { allExcept: ['snake_case'] } });
         });
 
         it('should report literal subscription', function() {
@@ -106,6 +92,28 @@ describe('rules/require-dot-notation', function() {
             assert(checker.checkString('var x = a[\'__camelA\']').getErrorCount() === 1);
             assert(checker.checkString('var x = a[\'camelA_\']').getErrorCount() === 1);
             assert(checker.checkString('var x = a[\'camelA__\']').getErrorCount() === 1);
+        });
+    });
+
+    describe('option value {"allExcept":"keywords"} with esnext option', function() {
+        beforeEach(function() {
+            checker.configure({ esnext: true, requireDotNotation: { allExcept: ['keywords'] } });
+        });
+
+        it('should not report literal subscription for es6 reserved words', function() {
+            assert(checker.checkString('var x = a[\'yield\']').isEmpty());
+            assert(checker.checkString('var x = a[\'let\']').isEmpty());
+        });
+    });
+
+    describe('deprecated option value "except_snake_case"', function() {
+        beforeEach(function() {
+            checker.configure({ requireDotNotation: 'except_snake_case' });
+        });
+
+        it('should report literal subscription', function() {
+            assert(checker.checkString('var x = a[\'b\']').getErrorCount() === 1);
+            assert(checker.checkString('var x = a[\'camelA\']').getErrorCount() === 1);
         });
     });
 });
