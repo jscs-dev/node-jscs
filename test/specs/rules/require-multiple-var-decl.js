8bea960249b0dbde9df8b593c9c0ad03ecb44cf4
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2015-07-23 22:05:18 -0700

    (require|disallow)MultipleVarDecl: Make obsolete

---
+++ test/specs/rules/require-multiple-var-decl.js
@@ -0,0 +1,21 @@
+var Checker = require('../../../lib/checker');
+var assert = require('assert');
+
+describe('rules/require-multiple-var-decl', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+    });
+
+    describe('obsolete', function() {
+        beforeEach(function() {
+            checker.configure({ requireMultipleVarDecl: true });
+        });
+
+        it('should report that the name is obsolete', function() {
+            assert(checker.checkString('void 0;').getErrorCount() === 1);
+        });
+    });
+});
