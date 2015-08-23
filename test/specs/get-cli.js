cf1ff5aaa30502e3af4bd50db7e419c185aa5bb7
Oleg Gaidarenko <markelog@gmail.com>
2015-08-23 20:17:15 +0300

    CLI: do not go into "recursion" when loading local version
    Ref gh-1719
    Fixes #1718

---
+++ test/specs/get-cli.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var path = require('path');
+
+var sinon = require('sinon');
+
+describe('lib/get-cli', function() {
+    describe('local with global load', function() {
+        function delCache() {
+            delete require.cache[path.resolve(__dirname, '../../lib/get-cli.js')];
+        }
+
+        beforeEach(delCache);
+        afterEach(delCache);
+
+        it('should load "global" version of jscs', function() {
+            var p = path.resolve(__dirname, '../../lib/get-cli.js');
+
+            assert(!require(p).test);
+        });
+
+        it('should load local version of "jscs"', function() {
+            sinon.stub(process, 'cwd', function() {
+                return path.resolve(__dirname, '../data/cli/modules');
+            });
+
+            assert(require('../../lib/get-cli.js').test);
+
+            process.cwd.restore();
+        });
+    });
+});
