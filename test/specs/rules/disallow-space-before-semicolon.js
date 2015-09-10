2ad2317eb55b38ef37078824ce73c41100f95d7a
Oleg Gaidarenko <markelog@gmail.com>
2015-09-10 08:02:46 +0300

    disallowSpaceBeforeSemicolon: add `allExcept` option
    Fixes #1640

--- test/specs/rules/disallow-space-before-semicolon.js
+++ test/specs/rules/disallow-space-before-semicolon.js
@@ -9,6 +9,24 @@ describe('rules/disallow-space-before-semicolon', function() {
         checker.registerDefaultRules();
     });
 
+    describe('invalid configuration', function() {
+        it('should not accept objects without at least one valid key', function() {
+            assert.throws(function() {
+                    checker.configure({ disallowSpaceBeforeSemicolon: {} });
+                },
+                assert.AssertionError
+            );
+        });
+
+        it('should not accept non-boolean non-objects', function() {
+            assert.throws(function() {
+                    checker.configure({ disallowSpaceBeforeSemicolon: 'true' });
+                },
+                assert.AssertionError
+            );
+        });
+    });
+
     it('does not allow spaces before semicolons', function() {
         checker.configure({ disallowSpaceBeforeSemicolon: true });
 
@@ -21,5 +39,15 @@ describe('rules/disallow-space-before-semicolon', function() {
         checker.configure({ disallowSpaceBeforeSemicolon: true });
 
         assert(checker.checkString('var a = 1;').isEmpty());
+    });
+
+    it('should allow space after parentheses', function() {
+        checker.configure({
+            disallowSpaceBeforeSemicolon: {
+                allExcept: ['(']
+            }
+        });
+
+        assert(checker.checkString('for ( ; nodeIndex < nodesCount; ++nodeIndex ) {}').isEmpty());
     });
 });
