85313ab073c6603d9869250af848c53872108539
Oleg Gaidarenko <markelog@gmail.com>
2015-09-13 18:17:22 +0300

    New rule: disallowUnusedParams
    Disallows unused params in function expression and function declaration

--- test/specs/rules/disallow-unused-params.js
+++ test/specs/rules/disallow-unused-params.js
@@ -13,12 +13,30 @@ describe('rules/disallow-unused-params', function() {
         checker.configure(config);
     });
 
+    it('should not report first argument is if second one is used', function() {
+        assert(checker.checkString('function a(b, c) { return c; };').isEmpty());
+    });
+
+    it('should not report on param that used in child scope', function() {
+        assert(checker.checkString(
+            'function a(b) { return function () { return b; }; }'
+        ).isEmpty());
+    });
+
     reportAndFix({
         name: 'function a(b) {}',
         rules: config,
         errors: 1,
         input: 'function a(b) {}',
         output: 'function a() {}'
+    });
+
+    reportAndFix({
+        name: 'slice(0, i);\nfunction a(obj, tail) {}',
+        rules: config,
+        errors: 2,
+        input: 'slice(0, i);\nfunction a(obj, tail) {}',
+        output: 'slice(0, i);\nfunction a() {}'
     });
 
     reportAndFix({
@@ -32,17 +50,17 @@ describe('rules/disallow-unused-params', function() {
     reportAndFix({
         name: 'function a(b, c) { return c; }',
         rules: config,
-        errors: 1,
+        errors: 0,
         input: 'function a(b, c) { return c; }',
-        output: 'function a(c) { return c; }'
+        output: 'function a(b, c) { return c; }'
     });
 
     reportAndFix({
-        name: 'function a(b, c) { var t = c; return t; }',
+        name: 'function a(b) { return function() { return b; }; }',
         rules: config,
-        errors: 1,
-        input: 'function a(b, c) { var t = c; return t; }',
-        output: 'function a(c) { var t = c; return t; }'
+        errors: 0,
+        input: 'function a(b, c) { return c; }',
+        output: 'function a(b, c) { return c; }'
     });
 
     reportAndFix({
@@ -58,22 +76,30 @@ describe('rules/disallow-unused-params', function() {
         rules: config,
         errors: 1,
         input: 'function a(b, c, d) { var t = c; var z = b; return {t: t, z: z}; }',
-        output: 'function a(b, c) { var t = c; var z = b; return {t: t, z: z}; }',
+        output: 'function a(b, c) { var t = c; var z = b; return {t: t, z: z}; }'
     });
 
     reportAndFix({
-        name: 'function a(b, c, d) { return c; }',
+        name: 'function a(b, c, d) { return b; }',
         rules: config,
         errors: 2,
-        input: 'function a(b, c, d) { return c; }',
-        output: 'function a(c) { return c; }',
+        input: 'function a(b, c, d) { return b; }',
+        output: 'function a(b) { return b; }'
     });
 
     reportAndFix({
         name: 'var a = function(b, c, d) { return c; }',
         rules: config,
-        errors: 2,
+        errors: 1,
         input: 'var a = function(b, c, d) { return c; }',
-        output: 'var a = function(c) { return c; }',
+        output: 'var a = function(b, c) { return c; }'
+    });
+
+    reportAndFix({
+        name: 'module.exports = function a(c, b) { return c; }',
+        rules: config,
+        errors: 1,
+        input: 'module.exports = function a(c, b) { return c; }',
+        output: 'module.exports = function a(c) { return c; }'
     });
 });
