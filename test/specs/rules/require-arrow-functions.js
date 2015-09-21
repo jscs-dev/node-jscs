71aba0eee0627e743ad8f8a25db3fafb75dab2eb
Henry Zhu <hi@henryzoo.com>
2015-09-21 09:22:36 -0400

    Fix: requireArrowFunctions to error on f(function () {}.bind(this))
    Fixes #1659
    Closes gh-1799

--- test/specs/rules/require-arrow-functions.js
+++ test/specs/rules/require-arrow-functions.js
@@ -4,6 +4,14 @@ var assert = require('assert');
 describe('rules/require-arrow-functions', function() {
     var checker;
 
+    function assertEmpty(str) {
+        assert(checker.checkString(str).isEmpty());
+    }
+
+    function assertOne(str) {
+        assert(checker.checkString(str));
+    }
+
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
@@ -11,87 +19,87 @@ describe('rules/require-arrow-functions', function() {
     });
 
     it('should report use of anonymous function expression in VariableDeclaration', function() {
-        assert(checker.checkString([
+        assertOne([
             'var anon = function(n) {',
                 'return n + 1;',
             '});'
-        ].join('\n')).getErrorCount() === 1);
+        ].join('\n'));
     });
 
     it('should report use of named function expression in VariableDeclaration', function() {
-        assert(checker.checkString([
+        assertOne([
             'var a = function named(n) {',
                 'return n + 1;',
             '});'
-        ].join('\n')).getErrorCount() === 1);
+        ].join('\n'));
     });
 
     it('should report use of function expression as callback', function() {
-        assert(checker.checkString([
+        assertOne([
             'a.map(function(n) {',
                 'return n + 1;',
             '});'
-        ].join('\n')).getErrorCount() === 1);
+        ].join('\n'));
     });
 
     it('should not report use of function expression in a ReturnStatement', function() {
-        assert(checker.checkString('function a() { return function() {} }').isEmpty());
+        assertEmpty('function a() { return function() {} }');
     });
 
     it('should not report use of object property function expression #1413', function() {
-        assert(checker.checkString([
+        assertEmpty([
             'var foo = {};',
             'foo.bar = function() {};'
-        ].join('\n')).isEmpty());
-        assert(checker.checkString([
+        ].join('\n'));
+        assertEmpty([
             'var foo = {',
               'bar: function() {}',
             '};'
-        ].join('\n')).isEmpty());
+        ].join('\n'));
     });
 
     it('should not report function expression in a AssignmentExpression', function() {
-        assert(checker.checkString('a.b = function() {}').isEmpty());
-        assert(checker.checkString('a.b.c = function() {}').isEmpty());
+        assertEmpty('a.b = function() {}');
+        assertEmpty('a.b.c = function() {}');
     });
 
     it('should not report a function declaration', function() {
-        assert(checker.checkString('function a(n) { return n + 1; }').isEmpty());
+        assertEmpty('function a(n) { return n + 1; }');
     });
 
     it('should not report a getter expression', function() {
-        assert(checker.checkString('var x = { get y() {} }').isEmpty());
+        assertEmpty('var x = { get y() {} }');
     });
 
     it('should not report a setter expression', function() {
-        assert(checker.checkString('var x = { set y(a) {} }').isEmpty());
+        assertEmpty('var x = { set y(a) {} }');
     });
 
     it('should not report a shorthand object method', function() {
-        assert(checker.checkString([
+        assertEmpty([
             'var foo = {',
               'bar() {}',
             '};'
-        ].join('\n')).isEmpty());
+        ].join('\n'));
     });
 
     it('should not report a class method', function() {
-        assert(checker.checkString([
+        assertEmpty([
             'class Foo {',
               'bar() {}',
             '};'
-        ].join('\n')).isEmpty());
+        ].join('\n'));
     });
 
     it('should not report use of arrow function', function() {
-        assert(checker.checkString('a.map(n => n + 1);').isEmpty());
+        assertEmpty('a.map(n => n + 1);');
     });
 
     it('should not report use of multi line arrow function', function() {
-        assert(checker.checkString([
+        assertEmpty([
             'a.map(n => {',
                 'return n + 1;',
             '});'
-        ].join('\n')).isEmpty());
+        ].join('\n'));
     });
 });
