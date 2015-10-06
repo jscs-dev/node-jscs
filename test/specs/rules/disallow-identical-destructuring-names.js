ae05218f249b268845a73786415235b9fb233f75
Henry Zhu <hi@henryzoo.com>
2015-10-06 09:42:56 -0400

    New Rule: disallowIdenticalDestructuringNames
    Fixes #1081
    Closes gh-1844

---
+++ test/specs/rules/disallow-identical-destructuring-names.js
@@ -0,0 +1,39 @@
+var Checker = require('../../../lib/checker');
+var assert = require('assert');
+
+describe('rules/disallow-identical-destructuring-names', function() {
+    var checker;
+
+    function assertNum(str, num) {
+        assert(checker.checkString(str).getErrorCount() === num);
+    }
+
+    function assertEmpty(str) {
+        assertNum(str, 0);
+    }
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+        checker.configure({ disallowIdenticalDestructuringNames: true });
+    });
+
+    it('should report identical destructuring names', function() {
+        assertNum('var { a: a } = obj;', 1);
+    });
+
+    it('should report multiple identical destructuring names', function() {
+        assertNum('var { a: a, b: b, c: c } = obj;', 3);
+    });
+
+    it('should report only identical destructuring names', function() {
+        assertNum('var { a, b: b } = obj;', 1);
+    });
+
+    it('should not report other destructuring', function() {
+        assertEmpty('var { a } = obj;');
+        assertEmpty('var { a, b, c } = obj;');
+        assertEmpty('var { a: a1, b: b2, c: c3 } = obj;');
+        assertEmpty('var { [a]: a } = obj;');
+    });
+});
