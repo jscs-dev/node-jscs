c842dfd6aa95c0b6f4cf5c1060a7072488a91558
Jeff Gulbronson <jeff.gulbronson@seeq.com>
2015-10-22 09:47:00 -0700

    Increased code coverage and documentation

--- test/specs/rules/require-aligned-multiline-params.js
+++ test/specs/rules/require-aligned-multiline-params.js
@@ -4,7 +4,28 @@ var expect = require('chai').expect;
 describe('rules/require-aligned-multiline-params', function() {
     var checker;
     var option;
-    function rules() { return { 'requireAlignedMultilineParams': option }; }
+    function rules() {
+        return { 'requireAlignedMultilineParams': option };
+    }
+
+    describe('when we pass invalid config options', function() {
+        beforeEach(function() {
+            checker = new Checker();
+            checker.registerDefaultRules();
+        });
+
+        it('should error when an invalid string is passed as the config option', function() {
+            expect(function() {
+                checker.configure({ 'requireAlignedMultilineParams': 'invalid' });
+            }).to.throw();
+        });
+
+        it('should error when an object is passed as the config option', function() {
+            expect(function() {
+                checker.configure({ 'requireAlignedMultilineParams': {} });
+            }).to.throw();
+        });
+    });
 
     describe('when we pass true as the config option', function() {
         beforeEach(function() {
