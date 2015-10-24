7a8ed9827c5e8ffd3783b68095ad03bdd4894378
Brian Dixon <bjdixon@gmail.com>
2015-10-24 04:42:33 -0400

    requireSpaceAfterComma
    New rule value: exceptTrailingCommas will allow commas to be followed by
    either } or ] as an alternative to a space.
    
    Fixes #1915

--- test/specs/rules/require-space-after-comma.js
+++ test/specs/rules/require-space-after-comma.js
@@ -7,67 +7,169 @@ describe('rules/require-space-after-comma', function() {
     beforeEach(function() {
         checker = new Checker();
         checker.registerDefaultRules();
-        checker.configure({ requireSpaceAfterComma: true });
     });
 
-    it('should report error when no space is given', function() {
-        expect(checker.checkString('var a,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
+    describe('option value true', function() {
+
+        beforeEach(function() {
+            checker.configure({ requireSpaceAfterComma: true });
+        });
+
+        it('should report error when no space is given', function() {
+            expect(checker.checkString('var a,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
+        });
+
+        it('should report error when space is given before but not after comma', function() {
+            expect(checker.checkString('var a ,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
+        });
+
+        it('should allow space after comma in var declaration', function() {
+            expect(checker.checkString('var a, b;')).to.have.no.errors();
+        });
+
+        it('should allow space after and before comma in var declaration', function() {
+            expect(checker.checkString('var a , b;')).to.have.no.errors();
+        });
+
+        it('should allow new line after comma in var declaration', function() {
+            expect(checker.checkString('var a,\nb,\nc;')).to.have.no.errors();
+        });
+
+        it('should report errors when no space is given in arrays', function() {
+            expect(checker.checkString('var a = [1,2,3,4];')).to.have.error.count.equal(3);
+        });
+
+        it('should report errors when space is given before but not after commas in arrays', function() {
+            expect(checker.checkString('var a = [1 ,2 ,3 ,4];')).to.have.error.count.equal(3);
+        });
+
+        it('should allow space after comma in arrays', function() {
+            expect(checker.checkString('var a = [1, 2, 3, 4];')).to.have.no.errors();
+        });
+
+        it('should allow space after and before comma in arrays', function() {
+            expect(checker.checkString('var a = [1 , 2 , 3 , 4];')).to.have.no.errors();
+        });
+
+        it('should report error when no space is given after trailing comma in array', function() {
+            expect(checker.checkString('var a = [1, 2, 3, 4,];')).to.have.error.count.equal(1);
+        });
+
+        it('should allow new line after comma in arrays', function() {
+            expect(checker.checkString('var a = [1,\n2,\n3];')).to.have.no.errors();
+        });
+
+        it('should report errors when no space is given in objects', function() {
+            expect(checker.checkString('var a = {x:1,y:2,z:3};')).to.have.error.count.equal(2);
+        });
+
+        it('should report error when no space is given after trailing comma in object', function() {
+            expect(checker.checkString('var a = {x:1, y:2, z:3,};')).to.have.error.count.equal(1);
+        });
+
+        it('should report errors when space is given before but not after commas in objects', function() {
+            expect(checker.checkString('var a = {x:1 ,y:2 ,z:3};')).to.have.error.count.equal(2);
+        });
+
+        it('should allow space after comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1, y: 2, z: 3};')).to.have.no.errors();
+        });
+
+        it('should allow space after and before comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1 , y: 2 , z: 3};')).to.have.no.errors();
+        });
+
+        it('should allow new line after comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1,\ny: 2,\nz: 3};')).to.have.no.errors();
+        });
+
+        it('should report errors when no space is given after trailing comma in object in array', function() {
+            expect(checker.checkString('var a = [{a:1, b:2,}, {c:3, d:4,},];')).to.have.error.count.equal(3);
+        });
+
     });
 
-    it('should report error when space is given before but not after comma', function() {
-        expect(checker.checkString('var a ,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
-    });
+    describe('option value "exceptTrailingCommas"', function() {
 
-    it('should allow space after comma in var declaration', function() {
-        expect(checker.checkString('var a, b;')).to.have.no.errors();
-    });
+        beforeEach(function() {
+            checker.configure({ requireSpaceAfterComma: 'exceptTrailingCommas' });
+        });
 
-    it('should allow space after and before comma in var declaration', function() {
-        expect(checker.checkString('var a , b;')).to.have.no.errors();
-    });
+        it('should report error when no space is given', function() {
+            expect(checker.checkString('var a,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
+        });
 
-    it('should allow new line after comma in var declaration', function() {
-        expect(checker.checkString('var a,\nb,\nc;')).to.have.no.errors();
-    });
+        it('should report error when space is given before but not after comma', function() {
+            expect(checker.checkString('var a ,b;')).to.have.one.validation.error.from('requireSpaceAfterComma');
+        });
 
-    it('should report errors when no space is given in arrays', function() {
-        expect(checker.checkString('var a = [1,2,3,4];')).to.have.error.count.equal(3);
-    });
+        it('should allow space after comma in var declaration', function() {
+            expect(checker.checkString('var a, b;')).to.have.no.errors();
+        });
 
-    it('should report errors when space is given before but not after commas in arrays', function() {
-        expect(checker.checkString('var a = [1 ,2 ,3 ,4];')).to.have.error.count.equal(3);
-    });
+        it('should allow space after and before comma in var declaration', function() {
+            expect(checker.checkString('var a , b;')).to.have.no.errors();
+        });
 
-    it('should allow space after comma in arrays', function() {
-        expect(checker.checkString('var a = [1, 2, 3, 4];')).to.have.no.errors();
-    });
+        it('should allow new line after comma in var declaration', function() {
+            expect(checker.checkString('var a,\nb,\nc;')).to.have.no.errors();
+        });
 
-    it('should allow space after and before comma in arrays', function() {
-        expect(checker.checkString('var a = [1 , 2 , 3 , 4];')).to.have.no.errors();
-    });
+        it('should report errors when no space is given in arrays', function() {
+            expect(checker.checkString('var a = [1,2,3,4];')).to.have.error.count.equal(3);
+        });
 
-    it('should allow new line after comma in arrays', function() {
-        expect(checker.checkString('var a = [1,\n2,\n3];')).to.have.no.errors();
-    });
+        it('should report errors when space is given before but not after commas in arrays', function() {
+            expect(checker.checkString('var a = [1 ,2 ,3 ,4];')).to.have.error.count.equal(3);
+        });
 
-    it('should report errors when no space is given in objects', function() {
-        expect(checker.checkString('var a = {x:1,y:2,z:3};')).to.have.error.count.equal(2);
-    });
+        it('should allow space after comma in arrays', function() {
+            expect(checker.checkString('var a = [1, 2, 3, 4];')).to.have.no.errors();
+        });
 
-    it('should report errors when space is given before but not after commas in objects', function() {
-        expect(checker.checkString('var a = {x:1 ,y:2 ,z:3};')).to.have.error.count.equal(2);
-    });
+        it('should allow space after and before comma in arrays', function() {
+            expect(checker.checkString('var a = [1 , 2 , 3 , 4];')).to.have.no.errors();
+        });
 
-    it('should allow space after comma in objects', function() {
-        expect(checker.checkString('var a = {x: 1, y: 2, z: 3};')).to.have.no.errors();
-    });
+        it('should allow space after trailing comma in arrays', function() {
+            expect(checker.checkString('var a = [1, 2, 3, 4, ];')).to.have.no.errors();
+        });
 
-    it('should allow space after and before comma in objects', function() {
-        expect(checker.checkString('var a = {x: 1 , y: 2 , z: 3};')).to.have.no.errors();
-    });
+        it('should allow no space after trailing comma in arrays', function() {
+            expect(checker.checkString('var a = [1, 2, 3, 4,];')).to.have.no.errors();
+        });
 
-    it('should allow new line after comma in objects', function() {
-        expect(checker.checkString('var a = {x: 1,\ny: 2,\nz: 3};')).to.have.no.errors();
-    });
+        it('should allow new line after comma in arrays', function() {
+            expect(checker.checkString('var a = [1,\n2,\n3];')).to.have.no.errors();
+        });
 
+        it('should report errors when no space is given in objects', function() {
+            expect(checker.checkString('var a = {x:1,y:2,z:3};')).to.have.error.count.equal(2);
+        });
+
+        it('should allow when no space is given after trailing comma in object', function() {
+            expect(checker.checkString('var a = {x:1, y:2, z:3,};')).to.have.no.errors();
+        });
+
+        it('should report errors when space is given before but not after commas in objects', function() {
+            expect(checker.checkString('var a = {x:1 ,y:2 ,z:3};')).to.have.error.count.equal(2);
+        });
+
+        it('should allow space after comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1, y: 2, z: 3};')).to.have.no.errors();
+        });
+
+        it('should allow space after and before comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1 , y: 2 , z: 3};')).to.have.no.errors();
+        });
+
+        it('should allow new line after comma in objects', function() {
+            expect(checker.checkString('var a = {x: 1,\ny: 2,\nz: 3};')).to.have.no.errors();
+        });
+
+        it('should allow when no space is given after trailing comma in object in array', function() {
+            expect(checker.checkString('var a = [{a:1, b:2,}, {c:3, d:4,},];')).to.have.no.errors();
+        });
+
+    });
 });
