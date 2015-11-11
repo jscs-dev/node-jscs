0ed74eae1ef7c3b8e0181a4706c888f18197d975
Brian Dixon <bjdixon@gmail.com>
2015-11-10 21:02:38 -0500

    disallowSpace(After|Before)Comma: add `allExcept: ['sparseArrays']`
    Fixes #1944
    Closes gh-1971

--- test/specs/rules/disallow-space-after-comma.js
+++ test/specs/rules/disallow-space-after-comma.js
@@ -51,7 +51,21 @@ describe('rules/disallow-space-after-comma', function() {
     });
 
     it('does allow sparse arrays', function() {
+        expect(checker.checkString('[a,,,b,c]')).to.have.no.errors();
+    });
+
+    it('does not allow spaces in sparse arrays', function() {
+        expect(checker.checkString('[a, , ,b,c]')).to.have.error.count.equal(2);
+    });
+
+    it('does allow spaces in sparse arrays when excepted', function() {
+        checker.configure({ disallowSpaceAfterComma: {allExcept: ['sparseArrays']}});
         expect(checker.checkString('[a, , ,b,c]')).to.have.no.errors();
+    });
+
+    it('does allow spaces in sparse arrays when excepted but not before values', function() {
+        checker.configure({ disallowSpaceAfterComma: {allExcept: ['sparseArrays']}});
+        expect(checker.checkString('[a, , , b, c]')).to.have.error.count.equal(2);
     });
 
     it('does not allow spaces after commas in objects', function() {
