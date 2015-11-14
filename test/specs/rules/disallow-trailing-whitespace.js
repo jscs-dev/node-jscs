ecb67187e6c74d5ee02888218c1a725fbc0ab21b
Luke Page <luke.a.page@gmail.com>
2015-11-14 23:02:25 +0000

    disallowTrailingWhitespace: fix autofix in the comment
    Fix autofix of multiple trailing spaces in a comment

--- test/specs/rules/disallow-trailing-whitespace.js
+++ test/specs/rules/disallow-trailing-whitespace.js
@@ -52,10 +52,34 @@ describe('rules/disallow-trailing-whitespace', function() {
     });
 
     reportAndFix({
+        name: 'illegal multiple-whitespace in comment block',
+        rules: rules,
+        errors: 1,
+        input: '/*\n *      \n */',
+        output: '/*\n *\n */'
+    });
+
+    reportAndFix({
+        name: 'illegal multiple-whitespace in comment block when ignoring empty lines',
+        rules: { disallowTrailingWhitespace: 'ignoreEmptyLines' },
+        errors: 1,
+        input: '/*\n *      \n\n */',
+        output: '/*\n *\n\n */'
+    });
+
+    reportAndFix({
         name: 'illegal whitespace in comment line',
         rules: rules,
         errors: 1,
         input: '// line 1 \n// line 2\n',
+        output: '// line 1\n// line 2\n'
+    });
+
+    reportAndFix({
+        name: 'illegal multiple-whitespace in comment line',
+        rules: rules,
+        errors: 1,
+        input: '// line 1      \n// line 2\n',
         output: '// line 1\n// line 2\n'
     });
 
