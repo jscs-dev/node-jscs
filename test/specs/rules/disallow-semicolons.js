ae5e55882edfc4b68696ac9d340cb4fde056f7f0
Oleg Gaidarenko <markelog@gmail.com>
2015-12-20 10:06:59 +0300

    disallowSemicolons: correct autofix tests

--- test/specs/rules/disallow-semicolons.js
+++ test/specs/rules/disallow-semicolons.js
@@ -32,6 +32,61 @@ describe('rules/disallow-semicolons', function() {
             'function c() {}',
             'd();'
         ].join('\n'))).to.have.error.count.equal(3);
+    });
+
+    it('should allow semicolons inline', function() {
+        expect(checker.checkString([
+            'for (var i = 0; i < l; i++) {',
+            'go()',
+            '}'
+        ].join('\n'))).to.have.no.errors();
+
+        reportAndFix({
+            name: 'for (var i = 0; i < l; i++) {}',
+            rules: config,
+            errors: 0,
+            input: 'for (var i = 0; i < l; i++) {}',
+            output: 'for (var i = 0; i < l; i++) {}'
+        });
+    });
+
+    it('should allow semicolons at start of line', function() {
+        expect(checker.checkString([
+            'var foo = function () {}',
+            ';[1, 2].forEach(foo)'
+        ].join('\n'))).to.have.no.errors();
+    });
+
+    describe('ignore needed semicolons', function() {
+        it('`for`', function() {
+            expect(checker.checkString('for (var j = 0,\nlength = arr.length;\nj < l; j++) {}')).to.have.no.errors();
+        });
+
+        it('`[`', function() {
+            expect(checker.checkString('"a"\n[0]')).to.have.no.errors();
+        });
+
+        it('`(`', function() {
+            expect(checker.checkString('a;\n(1,2)')).to.have.no.errors();
+        });
+    });
+
+    describe('autofix', function() {
+        reportAndFix({
+            name: ';[1, 2].forEach(foo)',
+            rules: config,
+            errors: 0,
+            input: ';[1, 2].forEach(foo)',
+            output: ';[1, 2].forEach(foo)'
+        });
+
+        reportAndFix({
+            name: 'var a = 1;\nvar b = 1;',
+            rules: config,
+            errors: 2,
+            input: 'var a = 1;\nvar b = 1;',
+            output: 'var a = 1\nvar b = 1'
+        });
 
         reportAndFix({
             name: 'var a = 1;',
@@ -56,66 +111,13 @@ describe('rules/disallow-semicolons', function() {
             input: 'd();',
             output: 'd()'
         });
-    });
-
-    it('should allow semicolons inline', function() {
-        expect(checker.checkString([
-            'for (var i = 0; i < l; i++) {',
-            'go()',
-            '}'
-        ].join('\n'))).to.have.no.errors();
 
         reportAndFix({
-            name: 'for (var i = 0; i < l; i++) {}',
+            name: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}',
             rules: config,
             errors: 0,
-            input: 'for (var i = 0; i < l; i++) {}',
-            output: 'for (var i = 0; i < l; i++) {}'
+            input: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}',
+            output: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}'
         });
-    });
-
-    it('should allow semicolons at start of line', function() {
-        expect(checker.checkString([
-            'var foo = function () {}',
-            ';[1, 2].forEach(foo)'
-        ].join('\n'))).to.have.no.errors();
-
-        reportAndFix({
-            name: ';[1, 2].forEach(foo)',
-            rules: config,
-            errors: 0,
-            input: ';[1, 2].forEach(foo)',
-            output: ';[1, 2].forEach(foo)'
-        });
-    });
-
-    reportAndFix({
-        name: 'var a = 1;\nvar b = 1;',
-        rules: config,
-        errors: 2,
-        input: 'var a = 1;\nvar b = 1;',
-        output: 'var a = 1\nvar b = 1'
-    });
-
-    describe('ignore needed semicolons', function() {
-        it('`for`', function() {
-            expect(checker.checkString('for (var j = 0,\nlength = arr.length;\nj < l; j++) {}')).to.have.no.errors();
-        });
-
-        it('`[`', function() {
-            expect(checker.checkString('"a"\n[0]')).to.have.no.errors();
-        });
-
-        it('`(`', function() {
-            expect(checker.checkString('a;\n(1,2)')).to.have.no.errors();
-        });
-    });
-
-    reportAndFix({
-        name: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}',
-        rules: config,
-        errors: 0,
-        input: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}',
-        output: 'for (var j = 0,\nlength = arr.length;\nj < l; j++) {}'
     });
 });
