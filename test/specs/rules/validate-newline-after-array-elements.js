787e80e7ce5387703ee5aae79fe7c806955514a5
Henry Zhu <hi@henryzoo.com>
2016-01-23 17:40:17 -0500

    validateNewlineAfterArrayElements: add autofix tests, fixup config issues

--- test/specs/rules/validate-newline-after-array-elements.js
+++ test/specs/rules/validate-newline-after-array-elements.js
@@ -1,5 +1,6 @@
 var Checker = require('../../../lib/checker');
 var expect = require('chai').expect;
+var reportAndFix = require('../../lib/assertHelpers').reportAndFix;
 
 describe('rules/validate-newline-after-array-elements', function() {
     var checker;
@@ -18,8 +19,71 @@ describe('rules/validate-newline-after-array-elements', function() {
     });
 
     describe('true option', function() {
+        var rules = { validateNewlineAfterArrayElements: true };
+
         beforeEach(function() {
-            checker.configure({ validateNewlineAfterArrayElements: true });
+            checker.configure(rules);
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line-line array with many elements',
+            rules: rules,
+            errors: 2,
+            input: 'var x = [\n  1, 2, 3\n];',
+            output: 'var x = [\n  1,\n 2,\n 3\n];'
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line array with many elements and one item on the first line',
+            rules: rules,
+            errors: 1,
+            input: 'var x = [0,\n' +
+                        '1,\n' +
+                        '2,\n' +
+                        '3\n' +
+                    '];\n',
+            output: 'var x = [\n' +
+                        '0,\n' +
+                        '1,\n' +
+                        '2,\n' +
+                        '3\n' +
+                    '];\n'
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line array with many elements and one item on the last line',
+            rules: rules,
+            errors: 1,
+            input: 'var x = [\n' +
+                        '1,\n' +
+                        '2,\n' +
+                        '3,\n' +
+                    '4];\n',
+            output: 'var x = [\n' +
+                        '1,\n' +
+                        '2,\n' +
+                        '3,\n' +
+                        '4\n' +
+                    '];\n'
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line array with many elements on each line',
+            rules: rules,
+            errors: 3,
+            input: 'var x = [\n' +
+                        '1, 1,\n' +
+                        '2, 2,\n' +
+                        '3, 3\n' +
+                    '];\n',
+            output: 'var x = [\n' +
+                        '1,\n' +
+                        ' 1,\n' +
+                        '2,\n' +
+                        ' 2,\n' +
+                        '3,\n' +
+                        ' 3\n' +
+                    '];\n'
         });
 
         it('should not report for empty array', function() {
@@ -34,10 +98,6 @@ describe('rules/validate-newline-after-array-elements', function() {
         it('should not report for single-line array with many elements', function() {
             expect(checker.checkString('var x = [1, 2];')).to.have.no.errors();
             expect(checker.checkString('var x = [1, 2, 3];')).to.have.no.errors();
-        });
-
-        it('should report for single-line array with many elements', function() {
-            expect(checker.checkString('var x = [\n  1, 2, 3\n];')).to.have.error.count.equal(2);
         });
 
         it('should not report for multi-line array without elements', function() {
@@ -58,36 +118,6 @@ describe('rules/validate-newline-after-array-elements', function() {
                 )).to.have.no.errors();
         });
 
-        it('should report for multi-line array with many elements and one item on the first line', function() {
-            expect(checker.checkString(
-                    'var x = [0,\n' +
-                        '1,\n' +
-                        '2,\n' +
-                        '3\n' +
-                    '];\n'
-                )).to.have.one.validation.error.from('validateNewlineAfterArrayElements');
-        });
-
-        it('should report for multi-line array with many elements and one item on the last line', function() {
-            expect(checker.checkString(
-                    'var x = [\n' +
-                        '1,\n' +
-                        '2,\n' +
-                        '3,\n' +
-                    '4];\n'
-                )).to.have.one.validation.error.from('validateNewlineAfterArrayElements');
-        });
-
-        it('should report for multi-line array with many elements on each line', function() {
-            expect(checker.checkString(
-                    'var x = [\n' +
-                        '1, 1,\n' +
-                        '2, 2,\n' +
-                        '3, 3\n' +
-                    '];\n'
-                )).to.have.error.count.equal(3);
-        });
-
         it('should not report for one-line array with many holes', function() {
             expect(checker.checkString(
                     'var x = [\n' +
@@ -100,8 +130,10 @@ describe('rules/validate-newline-after-array-elements', function() {
     });
 
     describe('with value 3', function() {
+        var rules = { validateNewlineAfterArrayElements: 3 };
+
         beforeEach(function() {
-            checker.configure({ validateNewlineAfterArrayElements: 3 });
+            checker.configure(rules);
         });
 
         it('should not report for one-line array with 3 elements', function() {
@@ -110,18 +142,38 @@ describe('rules/validate-newline-after-array-elements', function() {
                 )).to.have.no.errors();
         });
 
-        it('should report for multi-line array with 3 elements', function() {
-            expect(checker.checkString(
-                    'var x = [\n' +
+        reportAndFix({
+            name: 'should report for multi-line array with 3 elements',
+            rules: rules,
+            errors: 1,
+            input: 'var x = [\n' +
                     '  1, 2\n' +
+                    '];',
+            output: 'var x = [\n' +
+                    '  1,\n' +
+                    ' 2\n' +
                     '];'
-                )).to.have.one.validation.error.from('validateNewlineAfterArrayElements');
         });
     });
 
     describe('maximum 3', function() {
+        var rules = { validateNewlineAfterArrayElements: { maximum: 3 } };
+
         beforeEach(function() {
-            checker.configure({ validateNewlineAfterArrayElements: { maximum: 3 } });
+            checker.configure(rules);
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line array with 3 elements',
+            rules: rules,
+            errors: 1,
+            input: 'var x = [\n' +
+                    '  1, 2\n' +
+                    '];',
+            output: 'var x = [\n' +
+                    '  1,\n' +
+                    ' 2\n' +
+                    '];'
         });
 
         it('should not report for one-line array with 3 elements', function() {
@@ -129,33 +181,37 @@ describe('rules/validate-newline-after-array-elements', function() {
                     'var x = [1, 2, 3];'
                 )).to.have.no.errors();
         });
-
-        it('should report for multi-line array with 3 elements', function() {
-            expect(checker.checkString(
-                    'var x = [\n' +
-                    '  1, 2\n' +
-                    '];'
-                )).to.have.one.validation.error.from('validateNewlineAfterArrayElements');
-        });
     });
 
     describe('maximum 2, ignoreBrackets true', function() {
+        var rules = { validateNewlineAfterArrayElements: { maximum: 2, ignoreBrackets: true } };
+
         beforeEach(function() {
-            checker.configure({ validateNewlineAfterArrayElements: { maximum: 2, ignoreBrackets: true } });
+            checker.configure(rules);
         });
 
-        it('should report for one-line array with 3 elements', function() {
-            expect(checker.checkString(
-                    'var x = [1, 2, 3];'
-                )).to.have.error.count.equal(2);
+        reportAndFix({
+            name: 'should report for one-line array with 3 elements',
+            rules: rules,
+            errors: 2,
+            input: 'var x = [1, 2, 3];',
+            output: 'var x = [1,\n' +
+                    ' 2,\n' +
+                    ' 3];'
         });
 
-        it('should report for multi-line array with 3 elements', function() {
-            expect(checker.checkString(
-                    'var x = [0,\n' +
-                    '  1, 2, 3,\n' +
+        reportAndFix({
+            name: 'should report for multi-line array with 3 elements',
+            rules: rules,
+            errors: 2,
+            input: 'var x = [0,\n' +
+                    ' 1, 2, 3,\n' +
+                    '4];',
+            output: 'var x = [0,\n' +
+                    ' 1,\n' +
+                    ' 2,\n' +
+                    ' 3,\n' +
                     '4];'
-                )).to.have.error.count.equal(2);
         });
 
         it('should not report for multi-line array with elements on 2 lines', function() {
@@ -168,22 +224,30 @@ describe('rules/validate-newline-after-array-elements', function() {
     });
 
     describe('ignoreBrackets true', function() {
+        var rules = { validateNewlineAfterArrayElements: { ignoreBrackets: true } };
+
         beforeEach(function() {
-            checker.configure({ validateNewlineAfterArrayElements: { ignoreBrackets: true } });
+            checker.configure(rules);
+        });
+
+        reportAndFix({
+            name: 'should report for multi-line array with 3 elements',
+            rules: rules,
+            errors: 2,
+            input: 'var x = [0,\n' +
+                    ' 1, 2, 3,\n' +
+                    '4];',
+            output: 'var x = [0,\n' +
+                    ' 1,\n' +
+                    ' 2,\n' +
+                    ' 3,\n' +
+                    '4];'
         });
 
         it('should not report for one-line array with 3 elements', function() {
             expect(checker.checkString(
                     'var x = [1, 2, 3];'
                 )).to.have.no.errors();
-        });
-
-        it('should report for multi-line array with 3 elements', function() {
-            expect(checker.checkString(
-                    'var x = [0,\n' +
-                    '  1, 2, 3,\n' +
-                    '4];'
-                )).to.have.error.count.equal(2);
         });
 
         it('should not report for multi-line array with elements on 2 lines', function() {
