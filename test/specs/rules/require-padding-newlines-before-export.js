488ec0d7ee1609ec0a0930c4bbbd7ab49436c5f1
Brian Schemp <bschemp@gmail.com>
2016-01-28 17:00:33 -1000

    requirePaddingNewLinesBeforeExport: exclude if only statement in block
    Fixes #1883
    Closes gh-2106

--- test/specs/rules/require-padding-newlines-before-export.js
+++ test/specs/rules/require-padding-newlines-before-export.js
@@ -31,5 +31,9 @@ describe('rules/require-padding-newlines-before-export', function() {
             expect(checker.checkString('var a = 2;\nfoo.exports = a;')).to.have.no.errors();
             expect(checker.checkString('var a = 2;\nmodule.foo = a;')).to.have.no.errors();
         });
+
+        it('should not report missing padding if export is only statement in block', function() {
+            expect(checker.checkString('if (true) {\nmodule.exports = a;\n}')).to.have.no.errors();
+        });
     });
 });
