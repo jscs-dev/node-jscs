e5ad9c755d6986d39423371b61741d8271d31fa8
ikokostya <ikokostya@gmail.com>
2016-02-10 04:44:39 +0300

    New Rule: disallowSpacesInsideTemplateStringPlaceholders (ES6)

---
+++ test/specs/rules/disallow-spaces-inside-template-string-placeholders.js
@@ -0,0 +1,51 @@
+var Checker = require('../../../lib/checker');
+var expect = require('chai').expect;
+
+describe('rules/disallow-spaces-inside-template-string-placeholders', function() {
+    var checker;
+
+    beforeEach(function() {
+        checker = new Checker();
+        checker.registerDefaultRules();
+
+        checker.configure({
+            disallowSpacesInsideTemplateStringPlaceholders: true
+        });
+    });
+
+    it('should report with space after opening curly brace', function() {
+        expect(checker.checkString('`${ 1}`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+    });
+
+    it('should report with space before closing curly brace', function() {
+        expect(checker.checkString('`${1 }`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+    });
+
+    it('should report with spaces before opening and after closing curly braces', function() {
+        expect(checker.checkString('`${ 1 }`')).to.have.error.count.equal(2);
+    });
+
+    it('should report with more than one space in placeholder', function() {
+        expect(checker.checkString('`${  1}`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+    });
+
+    it('should report with space in second placeholder', function() {
+        expect(checker.checkString('`${1} + ${ 2}`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+    });
+
+    it('should not report with no spaces in placeholder', function() {
+        expect(checker.checkString('`${1}`')).to.have.no.errors();
+    });
+
+    it('should work with tagged template string', function () {
+        expect(checker.checkString('tag`${1}`')).to.have.no.errors();
+        expect(checker.checkString('tag`${1 }`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+        expect(checker.checkString('tag`${ 1}`')).to.have.one.validation.error
+            .from('disallowSpacesInsideTemplateStringPlaceholders');
+    });
+});
