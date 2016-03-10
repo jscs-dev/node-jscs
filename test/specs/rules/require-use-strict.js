8f9ac72fde91edc77f230db597f0b501a658f95d
Erik Vold <erikvvold@gmail.com>
2016-03-10 14:33:51 -0800

    review changes

--- test/specs/rules/require-use-strict.js
+++ test/specs/rules/require-use-strict.js
@@ -11,82 +11,75 @@ function makeChecker(rules) {
     checker.configure(rules);
     return checker;
 }
+var checker = null;
 
 describe('rules/require-use-strict', function() {
+    beforeEach(function() {
+        checker = makeChecker();
+    });
+
     it('should report if use strict is not present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('var a = 2;'))
           .to.have.one.validation.error.from('requireUseStrict');
     });
 
     it('should report if rule is not used', function() {
-        var checker = makeChecker({});
+        checker = makeChecker({});
         expect(checker.checkString('var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should report if use strict is not present, with func', function() {
-        var checker = makeChecker();
         expect(checker.checkString('var a = 2;' + funcX))
           .to.have.one.validation.error.from('requireUseStrict');
     });
 
     it('should not report if use strict is present, with funcX', function() {
-        var checker = makeChecker();
         expect(checker.checkString('\'use strict\';var a = 2;' + funcX))
           .to.have.no.errors();
     });
 
     it('should not report if use strict is present, with funcY', function() {
-        var checker = makeChecker();
         expect(checker.checkString('\'use strict\';var a = 2;' + funcY))
           .to.have.no.errors();
     });
 
     it('should not report if \'use strict\' is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('\'use strict\';var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if \'use strict\' is present with comment line', function() {
-        var checker = makeChecker();
         expect(checker.checkString('// test\n\'use strict\';var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if \'use strict\' is present with comment block', function() {
-        var checker = makeChecker();
         expect(checker.checkString('/*\ntest */\n\'use strict\';var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if "use strict" is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('"use strict";var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if "use strict"\\s is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('"use strict"; var a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if "use strict"\\n is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('"use strict";\nvar a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if "use strict"\\n\\n is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('"use strict";\n\nvar a = 2;'))
           .to.have.no.errors();
     });
 
     it('should not report if "use strict"\\n\\t is present', function() {
-        var checker = makeChecker();
         expect(checker.checkString('"use strict";\n\tvar a = 2;'))
           .to.have.no.errors();
     });
