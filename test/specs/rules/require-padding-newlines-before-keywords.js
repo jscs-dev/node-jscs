44e5187c53a262b79a85068979490a6a7101d11e
Henry Zhu <hi@henryzoo.com>
2016-03-17 11:33:34 -0400

    Fix: fixup skipped requirePaddingNewlinesBeforeKeywords test
    Closes gh-2179

--- test/specs/rules/require-padding-newlines-before-keywords.js
+++ test/specs/rules/require-padding-newlines-before-keywords.js
@@ -103,51 +103,50 @@ describe('rules/require-padding-newlines-before-keywords', function() {
 
         it('should not report on first expression in a block', function() {
             expect(checker.checkString(
-                    'function x() { return; }'
-                )).to.have.no.errors();
+                'function x() { return; }'
+            )).to.have.no.errors();
         });
 
         it('should not report in object definitions', function() {
             expect(checker.checkString(
-                    'var obj = {\n' +
-                    '  foo: function x() { return; }\n' +
-                    '};'
-                )).to.have.no.errors();
+                'var obj = {\n' +
+                '  foo: function x() { return; }\n' +
+                '};'
+            )).to.have.no.errors();
         });
 
         it('should not report when parameter inside a function', function() {
             expect(checker.checkString(
-                    'watchlist.on( "watch", function () {} );'
-                )).to.have.no.errors();
+                'watchlist.on( "watch", function () {} );'
+            )).to.have.no.errors();
         });
 
         it('should not report when assigned to a variable', function() {
             expect(checker.checkString(
-                    ' Router.prototype.getPath = function () {};'
-                )).to.have.no.errors();
+                ' Router.prototype.getPath = function () {};'
+            )).to.have.no.errors();
         });
 
         it('should not report when used inside closure', function() {
             expect(checker.checkString(
-                    '( function ( $ ) {} )( jQuery );'
-                )).to.have.no.errors();
+                '( function ( $ ) {} )( jQuery );'
+            )).to.have.no.errors();
         });
 
         it('should report on matching return statement', function() {
             expect(checker.checkString(
-                    'function x() { var a; return; }'
-                )).to.have.one.validation.error.from('requirePaddingNewlinesBeforeKeywords');
+                'function x() { var a; return; }'
+            )).to.have.one.validation.error.from('requirePaddingNewlinesBeforeKeywords');
         });
 
-        // TODO: fix this in CST
-        it.skip('should not report when returning a function', function() {
+        it('should not report when returning a function', function() {
             expect(checker.checkString(
-                    'function x() {\n' +
-                    '  var a = 0;\n' +
-                    '  \n' +
-                    '  return function() {};\n' +
-                    '}'
-                )).to.have.no.errors();
+                'function x() {\n' +
+                '  var a = 0;\n' +
+                '  \n' +
+                '  return function() {};\n' +
+                '}'
+            )).to.have.no.errors();
         });
     });
 });
