2939fbf5d4445bbdea55653db5d573bf2681e1a7
Oleg Gaidarenko <markelog@gmail.com>
2016-04-02 22:19:27 +0300

    Generator: skip tests for it (too slow)

--- test/specs/config/generator.js
+++ test/specs/config/generator.js
@@ -9,7 +9,8 @@ var Configuration = require('../../../lib/config/configuration');
 var ConfigGenerator = require('../../../lib/config/generator');
 var crockfordClone = require('../../data/configs/generator/crockfordClone');
 
-describe('lib/config/generator', function() {
+// Skip it, to slow at the moment
+describe.skip('lib/config/generator', function() {
     this.timeout(60000);
 
     var _path = path.resolve(__dirname, '../../../lib/config/generator.js');
